bfac2750e77cb2c5d81cf367138efddd44257c2d
Zireael07 <zi@mail.com>
2013-08-26 13:49:33 +0200

    more varied rooms!

--- data/rooms/pilar2.lua
+++ data/rooms/pilar2.lua
@@ -1,5 +1,5 @@
--- Veins of the Earth
--- Zireael
+-- ToME - Tales of Middle-Earth
+-- Copyright (C) 2009, 2010, 2011, 2012, 2013 Nicolas Casalini
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -13,17 +13,14 @@
 --
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
 
 return {
-[[#!!#######!!#]],
-[[!~~~~~~~~~~.!]],
-[[!~~~~~~~~~~~!]],
-[[!~~~~~~.~~~~!]],
-[[!~~~~~~~~~~~!]],
-[[####....#####]],
-[[!~~~~~~.~~~~!]],
-[[!~~~~~~~~~~~!]],
-[[!.~~~~~~~~~~!]],
-[[!.~~~~~~~~~~!]],
-[[#!!!!!#!!!!!#]],
+[[#!!!!!!!!#]],
+[[!.#.#.#..!]],
+[[!........!]],
+[[!.#.#.#..!]],
+[[#!!!!!!!!#]],
 }
