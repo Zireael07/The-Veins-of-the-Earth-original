008f8df01d7a1679f03a6e0d87204e69f5f8871f
Zireael07 <zi@mail.com>
2013-09-16 15:21:41 +0200

    fix #49

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
--- ToME - Tales of Middle-Earth
--- Copyright (C) 2009, 2010, 2011, 2012, 2013 Nicolas Casalini
+-- Veins of the Earth
+-- Copyright (C) 2013 Zireael
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -13,21 +13,20 @@
 --
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
--- Nicolas Casalini "DarkGod"
--- darkgod@te4.org
 
 name = "VotE"
 long_name = "The Veins of the Earth"
 short_name = "veins"
 author = { "Zireael", "x" }
 homepage = "https://github.com/Zireael07/The-Veins-of-the-Earth"
-version = {0,0,8}
+version = {0,0,12}
 engine = {1,0,4,"te4"}
 description = [[
 In DarkGod's words, "a fantasy d20-themed dungeon crawler".
 ]]
 starter = "mod.load"
+
+no_get_name = true
 
 show_only_on_cheat = false -- Example modules are not shown to normal players
 
