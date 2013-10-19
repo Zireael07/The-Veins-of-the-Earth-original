6640ccfd7a8e566ccbd4c37c702ccc520a5e666d
Zireael07 <zi@mail.com>
2013-10-19 10:57:36 +0200

    new PlayerDisplay now with bars!

--- class/PlayerDisplay.lua
+++ class/PlayerDisplay.lua
@@ -14,113 +14,132 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 require "engine.class"
-local Mouse = require "engine.Mouse"
 
 module(..., package.seeall, class.make)
 
-function _M:init()
-	self.font = core.display.newFont("/data/font/DroidSansMono.ttf", 14)
-	self.font_h = self.font:lineSkip()
-
-	self.mouse = Mouse.new()
-
-	local gw, gh = core.display.size()
-	self:resize(1, gh - 150, 234, 150)
+function _M:init(x, y, w, h, bgcolor, font, size)
+    self.display_x = x
+    self.display_y = y
+    self.w, self.h = w, h
+    self.bgcolor = bgcolor
+    self.font = core.display.newFont(font, size)
+    self:resize(x, y, w, h)
 end
 
 --- Resize the display area
 function _M:resize(x, y, w, h)
-	self.display_x, self.display_y = x, y
-	self.mouse.delegate_offset_x = x
-	self.mouse.delegate_offset_y = y
-	self.w, self.h = w, h
-	self.font_w = self.font:size(" ")
-   self.surface_line = core.display.newSurface(w, self.font_h)
-	self.bars_x = self.font_w * 9
-	self.bars_w = self.w - self.bars_x - 5
-	self.surface = core.display.newSurface(w, h)
-	self.texture, self.texture_w, self.texture_h = self.surface:glTexture()
+    self.display_x, self.display_y = x, y
+    self.w, self.h = w, h
+    self.font_h = self.font:lineSkip()
+    self.font_w = self.font:size(" ")
+    self.bars_x = self.font_w * 6
+    self.bars_w = self.w - self.bars_x - 5
+    self.surface = core.display.newSurface(w, h)
+    self.surface_line = core.display.newSurface(w, self.font_h)
+    self.texture = self.surface:glTexture()
 
-	self.items = {}
-end
-
-function _M:mouseTooltip(text, w, h, x, y, click)
---	self.mouse:registerZone(x, y, w, h, function(button, mx, my, xrel, yrel, bx, by, event)
---		game.tooltip_x, game.tooltip_y = 1, 1; game.tooltip:displayAtMap(nil, nil, game.w, game.h, text)
---		if click and event == "button" and button == "left" then
---			click()
---		end
---	end)
-	return w, h
+    self.items = {}
 end
 
 function _M:makeTexture(text, x, y, r, g, b, max_w)
-	local s = self.surface
-	s:drawColorStringBlended(self.font, text, x, y, r, g, b, true, max_w)
-	return self.font:size(text)
+    local s = self.surface_line
+    s:erase(0, 0, 0, 0)
+    s:drawColorStringBlended(self.font, text, 0, 0, r, g, b, true, max_w)
+
+    local item = { s:glTexture() }
+    item.x = x
+    item.y = y
+    item.w = self.w
+    item.h = self.font_h
+    self.items[#self.items+1] = item
+
+    return item.w, item.h, item.x, item.y
 end
 
-function _M:makeTextureBar(text, nfmt, val, max, x, y, r, g, b, bar_col, bar_bgcol)
-	local s = self.surface_line
-	s:erase(0, 0, 0, 0)
-	s:erase(bar_bgcol.r, bar_bgcol.g, bar_bgcol.b, 255, self.bars_x, h, self.bars_w, self.font_h)
-	s:erase(bar_col.r, bar_col.g, bar_col.b, 255, self.bars_x, h, self.bars_w * val / max, self.font_h)
+function _M:makeTextureBar(text, nfmt, val, max, reg, x, y, r, g, b, bar_col, bar_bgcol)
+    local s = self.surface_line
+    s:erase(0, 0, 0, 0)
+    s:erase(bar_bgcol.r, bar_bgcol.g, bar_bgcol.b, 255, self.bars_x, h, self.bars_w, self.font_h)
+    s:erase(bar_col.r, bar_col.g, bar_col.b, 255, self.bars_x, h, self.bars_w * val / max, self.font_h)
 
-	s:drawColorStringBlended(self.font, text, 0, 0, r, g, b, true)
-	s:drawColorStringBlended(self.font, (nfmt or "%d/%d"):format(val, max), self.bars_x, 0, r, g, b)
+    s:drawColorStringBlended(self.font, text, 0, 0, r, g, b, true)
+    s:drawColorStringBlended(self.font, (nfmt or "%d/%d"):format(val, max), self.bars_x + 5, 0, r, g, b)
+    if reg and reg ~= 0 then
+        local reg_txt = (" (%s%.2f)"):format((reg > 0 and "+") or "",reg)
+        local reg_txt_w = self.font:size(reg_txt)
+        s:drawColorStringBlended(self.font, reg_txt, self.bars_x + self.bars_w - reg_txt_w - 3, 0, r, g, b)
+    end
+    local item = { s:glTexture() }
+    item.x = x
+    item.y = y
+    item.w = self.w
+    item.h = self.font_h
+    self.items[#self.items+1] = item
 
-	local item = { s:glTexture() }
-	item.x = x
-	item.y = y
-	item.w = self.w
-	item.h = self.font_h
-	self.items[#self.items+1] = item
-
-	return item.w, item.h, item.x, item.y
+    return item.w, item.h, item.x, item.y
 end
 
 -- Displays the stats
 function _M:display()
-	local player = game.player
-	if not player or not player.changed or not game.level then return end
+    local player = game.player
+    if not player or not player.changed or not game.level then return end
 
-	self.mouse:reset()
-	self.items = {}
+    self.items = {}
 
-	local s = self.surface
-	s:erase(0, 0, 0, 0)
+    local h = 6
+    local x = 2
 
+    self.font:setStyle("bold")
+    self:makeTexture(("%s#{normal}#"):format(player.name), 0, h, colors.GOLD.r, colors.GOLD.g, colors.GOLD.b, self.w) h = h + self.font_h
+    self.font:setStyle("normal")
+
+    h = h + self.font_h
+
+    self:makeTexture(("Lvl: #GOLD#%2d"):format(player.level), x, h, 255, 255, 255) h = h + self.font_h
     local cur_exp, max_exp = player.exp, player:getExpChart(player.level+1)
-    local gw, gh = core.display.size()
-	local w = 4  h = 1
-
-    self:makeTexture(("%-20s"):format(player.name), w, 0, 210, 210, 210)
-
-    h = h + self.font_h +2
-   
-
---    self:makeTextureBar("Exp:", "%d%%", cur_exp, 100, w, h, 255, 255, 255, { r=0, g=100, b=0 }, { r=0, g=50, b=0 }) h = h + self.font_h
-
-   self:makeTexture(("Level / Exp: #44dd44#%d / %2d%%"):format(player.level, 100 * cur_exp / max_exp), w, h, 255, 255, 255)
-   
-    h = h + self.font_h +2
+    local exp = cur_exp / max_exp * 100
     
---    self:makeTextureBar("Life:", nil, player.life, player.max_life, w, 5, 255, 255, 255, colors.DARK_RED, colors.VERY_DARK_RED)
+    self:makeTextureBar("Exp:", "%d%%", exp, 100, nil, x, h, 255, 255, 255, { r=0, g=100, b=0 }, { r=0, g=50, b=0 }) h = h + self.font_h
 
-    self:makeTexture(("Hit Points: #CRIMSON#%d/%d / %2d%%"):format(math.floor(player.life), math.floor(player.max_life), 100 * math.floor(player.life) / math.floor(player.max_life)), w, h, 255, 255, 255)
-   
-    if player.life < player.max_life*0.5 then self:makeTexture(("Hit Points: #CRIMSON#%d/%d / %2d%% †"):format(math.floor(player.life), math.floor(player.max_life), 100 * math.floor(player.life) / math.floor(player.max_life)), w, h, 255, 255, 255) end
+    h = h + self.font_h 
 
-    h = h + self.font_h +2
-   
-	self:makeTexture(("Dungeon lvl: %d"):format(game.level.level), w, h, 240, 240, 170)
+    if player.life < player.max_life*0.3 then self:makeTextureBar("#FIREBRICK#Life:", nil, player.life, player.max_life, nil, x, h, 255, 255, 255, colors.FIREBRICK, colors.VERY_DARK_RED) h = h + self.font_h
+    elseif player.life < player.max_life*0.5 then self:makeTextureBar("#DARK_RED#Life:", nil, player.life, player.max_life, nil, x, h, 255, 255, 255, colors.DARK_RED, colors.VERY_DARK_RED) h = h + self.font_h
+    else self:makeTextureBar("#CRIMSON#Life:", nil, player.life, player.max_life, nil, x, h, 255, 255, 255, colors.CRIMSON, colors.VERY_DARK_RED) h = h + self.font_h end
 
-	s:updateTexture(self.texture)
+    h = h + self.font_h
+
+    self:makeTexture(("Dungeon lvl: %d"):format(game.level.level), x, h, 240, 240, 170)
+
+    h = h + self.font_h
+
+    if savefile_pipe.saving then
+        h = h + self.font_h
+        self:makeTextureBar("Saving:", "%d%%", 100 * savefile_pipe.current_nb / savefile_pipe.total_nb, 100, nil, x, h, colors.YELLOW.r, colors.YELLOW.g, colors.YELLOW.b, 
+        {r=49, g=54,b=42},{r=17, g=19, b=0})
+
+        h = h + self.font_h
+    end
 end
 
-function _M:toScreen()
-	self:display()
-	self.texture:toScreenFull(self.display_x, self.display_y, self.w, self.h, self.texture_w, self.texture_h)
+function _M:toScreen(nb_keyframes)
+    self:display()
+    
+    core.display.drawQuad(self.display_x, self.display_y, self.w, self.h, 0, 0, 0, 0)
+    
+    for i = 1, #self.items do
+        local item = self.items[i]
+        if type(item) == "table" then
+            if item.glow then
+                local glow = (1+math.sin(core.game.getTime() / 500)) / 2 * 100 + 120
+                item[1]:toScreenFull(self.display_x + item.x, self.display_y + item.y, item.w, item.h, item[2], item[3], 1, 1, 1, glow / 255)
+            else
+                item[1]:toScreenFull(self.display_x + item.x, self.display_y + item.y, item.w, item.h, item[2], item[3])
+            end
+        else
+            item(self.display_x, self.display_y)
+        end
+    end
 end
+
