ffac4dc26f828f41163c9413af0e01f0950097fe
Zireael07 <zi@mail.com>
2013-10-20 17:37:21 +0200

    fix #56

--- class/Game.lua
+++ class/Game.lua
@@ -179,6 +179,54 @@ function _M:changeLevel(lev, zone)
 		self.player:move(self.level.default_down.x, self.level.default_down.y, true)
 	end
 	self.level:addEntity(self.player)
+
+	--Level feeling
+	local player = self.player
+	local feeling
+	local cr = 0
+	local max_cr = 0
+	if self.level.special_feeling then
+		feeling = self.level.special_feeling
+	else
+
+	for uid, e in pairs(game.level.entities) do --list[#list+1] = e 
+	cr = e.challenge
+	if cr > max_cr then max_cr = cr 
+	else end
+	end
+
+	local magic = 0
+	local max_magic = 0
+
+	--Detect powerful magic items
+--[[	for _, o in pairs(game.level.objects) do --list[#list+1] = o
+		local magic_bonus = o.combat.magic_bonus
+		local magic_armor = o.wielder.combat_magic_armor
+		local magic_shield = o.wielder.combat_magic_shield
+		local natural = o.wielder.combat_natural
+		local protection = self.wielder.combat_protection
+
+		if o.type == "weapon" and magic_bonus and magic_bonus > 0 then magic = o.magic_bonus
+		elseif o.type == "armor" and magic_armor and magic_armor > 0 then magic = o.magic_armor
+		elseif o.type == "shield" and magic_shield and magic_shield > 0 then magic = o.magic_shield
+		elseif o.type == "amulet" and natural and natural > 0 then magic = o.natural
+		elseif o.type == "ring" and protection and protection > 0 then magic = o.protection
+		else end
+
+		if magic > max_magic then max_magic = magic
+		else end
+	end
+
+		if max_magic > 2 then feeling = "You get goosebumps on your skin. A magic item is radiating power."
+		elseif max_magic > 4 then feeling = "The feeling of power threatens to overwhelm you. A powerful magic item must be nearby."]]
+
+		if max_cr > player.level + 4 then feeling = "You get the feeling that there is a powerful enemy here."
+		elseif max_cr < player.level -4 then feeling = "You feel very confident in your power."
+		else feeling = "You walk cautiously, feeling slightly anxious."	
+		end
+	end
+	if feeling then self.log("#TEAL#%s", feeling) end
+
 end
 
 function _M:getPlayer()
