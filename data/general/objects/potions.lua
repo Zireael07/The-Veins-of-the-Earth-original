8459cabfb274a6d3fccf1de8b261282554f0863f
Zireael07 <zi@mail.com>
2013-11-05 12:52:07 +0100

    Update potions.lua

--- data/general/objects/potions.lua
+++ data/general/objects/potions.lua
@@ -20,7 +20,7 @@ newEntity{
     identified = false,
     level_range = {1,10},
     cost = 50,
-    use_simple = { name = "cure light wounds",
+    use_simple = { name = "quaff",
     use = function(self,who)
         who:heal(rng.dice(1,8) + 5)
         return {used = true, destroy = true}
@@ -35,7 +35,7 @@ newEntity{
     identified = false,
     level_range = {4,10},
     cost = 300,
-    use_simple = { name = "cure moderate wounds",
+    use_simple = { name = "quaff",
     use = function(self,who)
         who:heal(rng.dice(2,8) + 5)
         return {used = true, destroy = true}
@@ -49,8 +49,8 @@ newEntity{
     unided_name = "a potion",
     identified = false,
     level_range = {1,10},
-    cost = 50,
-    use_simple = { name = "heal light wounds",
+    cost = 750,
+    use_simple = { name = "quaff",
     use = function(self,who)
         who:heal(who.max_life*0.1)
         return {used = true, destroy = true}
@@ -64,8 +64,8 @@ newEntity{
     unided_name = "a potion",
     identified = false,
     level_range = {1,10},
-    cost = 50,
-    use_simple = { name = "heal moderate wounds",
+    cost = 900,
+    use_simple = { name = "quaff",
     use = function(self,who)
         who:heal(who.max_life*0.3)
         return {used = true, destroy = true}
@@ -75,34 +75,34 @@ newEntity{
 
 newEntity{
     base = "BASE_POTION",
-    name = "a potion of heal moderate wounds",
+    name = "a potion of heal serious wounds",
     unided_name = "a potion",
     identified = false,
     level_range = {1,10},
-    cost = 50,
-    use_simple = { name = "heal moderate wounds",
+    cost = 1200,
+    use_simple = { name = "quaff",
     use = function(self,who)
-        who:heal(who.max_life*0.3)
+        who:heal(who.max_life*0.5)
         return {used = true, destroy = true}
   end
   }, 
 }
 
 
---newEntity{
---    base = "BASE_POTION",
---   name = "a potion of inflict light wounds",
---    unided_name = "a potion",
---    identified = false,
---    level_range = {1,10},
---    cost = 50,
---    use_simple = { name = "heal light wounds",
---    use = function(self,who)
---        who:heal(who.max_life*0.1)
---        return {used = true, destroy = true}
---  end
---  }, 
---}
+newEntity{
+    base = "BASE_POTION",
+    name = "a potion of inflict light wounds",
+    unided_name = "a potion",
+    identified = false,
+    level_range = {1,10},
+    cost = 50,
+    use_simple = { name = "quaff",
+    use = function(self,who)
+        who:heal(-who.max_life*0.1)
+        return {used = true, destroy = true}
+ end
+  }, 
+}
 
 newEntity{
     base = "BASE_POTION",
@@ -110,10 +110,116 @@ newEntity{
     unided_name = "a potion",
     identified = false,
     level_range = {1,10},
-    cost = 50,
-    use_simple = { name = "heal moderate wounds",
+    cost = 300,
+    use_simple = { name = "quaff",
     use = function(self,who)
         who:setEffect(self.EFF_BEAR_ENDURANCE, 5, {})
+        return {used = true, destroy = true}
+  end
+  }, 
+}
+
+newEntity{
+    base = "BASE_POTION",
+    name = "a potion of bull strength",
+    unided_name = "a potion",
+    identified = false,
+    level_range = {1,10},
+    cost = 300,
+    use_simple = { name = "quaff",
+    use = function(self,who)
+        who:setEffect(self.EFF_BULL_STRENGTH, 5, {})
+        return {used = true, destroy = true}
+  end
+  }, 
+}
+
+newEntity{
+    base = "BASE_POTION",
+    name = "a potion of eagle splendor",
+    unided_name = "a potion",
+    identified = false,
+    level_range = {1,10},
+    cost = 300,
+    use_simple = { name = "quaff",
+    use = function(self,who)
+        who:setEffect(self.EFF_EAGLE_SPLENDOR, 5, {})
+        return {used = true, destroy = true}
+  end
+  }, 
+}
+
+newEntity{
+    base = "BASE_POTION",
+    name = "a potion of owl wisdom",
+    unided_name = "a potion",
+    identified = false,
+    level_range = {1,10},
+    cost = 300,
+    use_simple = { name = "quaff",
+    use = function(self,who)
+        who:setEffect(self.EFF_OWL_WISDOM, 5, {})
+        return {used = true, destroy = true}
+  end
+  }, 
+}
+
+newEntity{
+    base = "BASE_POTION",
+    name = "a potion of cat grace",
+    unided_name = "a potion",
+    identified = false,
+    level_range = {1,10},
+    cost = 300,
+    use_simple = { name = "quaff",
+    use = function(self,who)
+        who:setEffect(self.EFF_CAT_GRACE, 5, {})
+        return {used = true, destroy = true}
+  end
+  }, 
+}
+
+newEntity{
+    base = "BASE_POTION",
+    name = "a potion of fox cunning",
+    unided_name = "a potion",
+    identified = false,
+    level_range = {1,10},
+    cost = 300,
+    use_simple = { name = "quaff",
+    use = function(self,who)
+        who:setEffect(self.EFF_FOX_CUNNING, 5, {})
+        return {used = true, destroy = true}
+  end
+  }, 
+}
+
+newEntity{
+    base = "BASE_POTION",
+    name = "a potion of mage armor",
+    unided_name = "a potion",
+    identified = false,
+    level_range = {1,10},
+    cost = 50,
+    use_simple = { name = "quaff",
+    use = function(self,who)
+        who:setEffect(self.EFF_MAGE_ARMOR, 5, {})
+        return {used = true, destroy = true}
+  end
+  }, 
+}
+
+
+newEntity{
+    base = "BASE_POTION",
+    name = "a potion of poison",
+    unided_name = "a potion",
+    identified = false,
+    level_range = {1,10},
+    cost = 50,
+    use_simple = { name = "quaff",
+    use = function(self,who)
+        who:setEffect(self.EFF_POISON1d10CON, 6, {})
         return {used = true, destroy = true}
   end
   }, 
