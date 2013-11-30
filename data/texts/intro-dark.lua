1cebc10bb41f99bef497d7d3d16897d5b0d2bc95
Zireael07 <zi@mail.com>
2013-11-30 15:57:54 +0100

    fix intro spacing

--- data/texts/intro-dark.lua
+++ data/texts/intro-dark.lua
@@ -1,4 +1,3 @@
 return [[You were leading a war party in the wilds. However, things took a turn for the worse and you are now the only survivor.
 Brave adventurer, you are now lost in the underground corridors of the #LIGHT_BROWN#Veins of the Earth#LAST#. There is no way to return to your homeland.
-
 How long can you survive?]]
--- data/texts/intro-outcast.lua
+++ data/texts/intro-outcast.lua
@@ -1,5 +1,3 @@
 return [[You have been cast out of your birth city and house because of your ideals. 
-
 Brave adventurer, you are now lost in the underground corridors of the #LIGHT_BROWN#Veins of the Earth#LAST#. There is no way to return to your homeland.
-
 How long can you survive?]]
