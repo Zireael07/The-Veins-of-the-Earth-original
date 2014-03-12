446924940ecd9381dd36c68cf97edf4320084434
Zireael07 <zi@mail.com>
2014-03-12 20:52:27 +0100

    new digging implementation which works

--- data/damage_types.lua
+++ data/damage_types.lua
@@ -189,23 +189,18 @@ newDamageType{
 --Enable digging
 
 newDamageType{
-	name = "dig", type = "DIG",
-	projector = function(src, x, y, typ, dam)
-		local feat = game.level.map(x, y, Map.TERRAIN)
-		if feat then
-			if feat.dig then
-				local newfeat_name, newfeat, silence = feat.dig, nil, false
-				if type(feat.dig) == "function" then newfeat_name, newfeat, silence = feat.dig(src, x, y, feat) end
-				newfeat = newfeat or game.zone.grid_list[newfeat_name]
-				if newfeat then
-					game.level.map(x, y, Map.TERRAIN, newfeat)
-					src.dug_times = (src.dug_times or 0) + 1
-					game.nicer_tiles:updateAround(game.level, x, y)
-					if not silence then
-						game.logSeen({x=x,y=y}, "%s turns into %s.", feat.name:capitalize(), newfeat.name)
-					end
-				end
-			end
-		end
-	end,
+    name = "dig", type = "DIG",
+    projector = function(src, x, y, typ, dam)
+        local feat = game.level.map(x, y, Map.TERRAIN)
+        if not feat or not feat.dig then return end
+
+        local newfeat_name, newfeat, silence = feat.dig, nil, false
+        if type(feat.dig) == "function" then newfeat_name, newfeat, silence = feat.dig(src, x, y, feat) end
+        game.level.map(x, y, Map.TERRAIN, newfeat or game.zone.grid_list[newfeat_name])
+        if not silence then
+            game.logSeen({x=x,y=y}, "The %s turns into %s.", feat.name, (newfeat or game.zone.grid_list[newfeat_name]).name)
+        end
+
+        if src.talentCallbackAllOn then src:talentCallbackAllOn("on_dig", x, y, feat) end
+    end,
 }
--- data/general/objects/pickaxes.lua
+++ data/general/objects/pickaxes.lua
@@ -12,9 +12,66 @@ newEntity{
 	encumber = 3,
 	rarity = 20,
 	desc = [[Allows you to dig.]],
-	carrier = {
+--[[	carrier = {
 		learn_talent = { [Talents.T_DIG] = 1, },
-	},
+	},]]
+
+	--Based on Qi Daozei
+	digspeed = 10,
+    use_no_wear = true,
+    use_no_energy = true, -- energy cost is handled by wait() below
+
+    getEffectiveDigSpeed = function(self, who, show_message)
+       --[[ local Talents = require "engine.interface.ActorTalents"
+        local mining = (who:getDex()-10)/2
+        if mining == 0 then
+            if show_message then game.logPlayer(who, "You don't know the first thing about mining. This could take a while...") end
+            ]]
+            return math.floor(self.digspeed * 2)
+        --[[else
+            return math.floor(self.digspeed * math.pow(0.9, mining - 1))
+        end]]
+    end,
+
+    -- Based on ToME's DIG_OBJECT
+    use_simple = {
+        name = "dig",
+        use = function(self, who)
+            local Map = require "engine.Map"
+            local tg = {type="bolt", range=1, nolock=true}
+            local x, y = who:getTarget(tg)
+            if not x or not y then return nil end
+            local feat = game.level.map(x, y, Map.TERRAIN)
+            if not feat.dig then
+                game.logPlayer(who, ("The %s is not diggable."):format(feat.name))
+                return nil
+            end
+
+            -- Hack: Subtracting 1 is necessary to make turns taken match.
+            -- I'm not sure why.
+            local digspeed = self:getEffectiveDigSpeed(who, true) - 1
+
+            local wait = function()
+                local co = coroutine.running()
+                local ok = false
+                who:restInit(digspeed, "digging", "dug", function(cnt, max)
+                    if cnt > max then ok = true end
+                    coroutine.resume(co)
+                end)
+                coroutine.yield()
+                if not ok then
+                    game.logPlayer(who, "You have been interrupted!")
+                    return false
+                end
+                return true
+            end
+            if wait() then
+                who:project(tg, x, y, engine.DamageType.DIG, 1)
+            end
+
+            return true
+        end,
+    }
 }
 
 newEntity{ base = "BASE_DIGGER",
