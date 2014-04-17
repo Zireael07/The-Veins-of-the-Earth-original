a516052b3119d692be807e86c9107624b1d9aa80
Zireael07 <zi@mail.com>
2014-04-17 19:17:28 +0200

    trying to give store some equipment

--- data/general/stores/general.lua
+++ data/general/stores/general.lua
@@ -5,6 +5,23 @@ newEntity{
 	store = {
 		purse = 10000,
 		empty_before_restock = false,
-		
+		filters = {
+			{type="weapon", subtype="shortsword", id=true },
+			{type="weapon", subtype="sword", id=true, },
+			{type="weapon", subtype="waraxe", id=true, },
+			{type="weapon", subtype="battleaxe", id=true, },
+			{type="weapon", subtype="mace", id=true,},
+			{type="weapon", subtype="dagger", id=true, },
+			{type="weapon", subtype="staff", id=true, },
+			{type="weapon", subtype="crossbow", id=true, },
+			{type="weapon", subtype="bow", id=true, },
+			{type="weapon", subtype="sling", id=true, },
+			{type="ammo", id=true, },
+			{type="ammo", id=true, },
+			{type="armor", subtype="heavy", id=true, },
+			{type="armor", subtype="shield", id=true, },
+			{type="armor", subtype="light", id=true, },
+			{type="armor", subtype="medium", id=true, },
+		},
 	},
 }
