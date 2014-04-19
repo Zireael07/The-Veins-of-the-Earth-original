67cd38bd98a1b27e4a37ad9f0a13e3a5acda2d39
Zireael07 <zi@mail.com>
2014-04-19 18:08:46 +0200

    add prompt to intro messages & no more covering by the border

--- data/texts/intro-dark.lua
+++ data/texts/intro-dark.lua
@@ -1,3 +1,7 @@
 return [[You were leading a war party in the wilds. However, things took a turn for the worse and you are now the only survivor.
 Brave adventurer, you are now lost in the underground corridors of the #LIGHT_BROWN#Veins of the Earth#LAST#. There is no way to return to your homeland.
-How long can you survive?]]
+How long can you survive?
+
+Press ESC or click to close.
+
+]]
--- data/texts/intro-outcast.lua
+++ data/texts/intro-outcast.lua
@@ -1,3 +1,7 @@
 return [[You have been cast out of your birth city and house because of your ideals. 
 Brave adventurer, you are now lost in the underground corridors of the #LIGHT_BROWN#Veins of the Earth#LAST#. There is no way to return to your homeland.
-How long can you survive?]]
+How long can you survive?
+
+Press ESC or click to close.
+
+]]
