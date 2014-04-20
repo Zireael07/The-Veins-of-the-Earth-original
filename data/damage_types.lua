c29df9ad9f33ec894c1e1002abc742d1ae75bfd6
Zireael07 <zi@mail.com>
2014-04-20 12:49:09 +0200

    Revert "remove one more vestige of flasher"
    This reverts commit 1e886d516433b100a14ff30143a4538d80b474b9.

--- data/damage_types.lua
+++ data/damage_types.lua
@@ -21,13 +21,11 @@
 setDefaultProjector(function(src, x, y, type, dam)
 	local target = game.level.map(x, y, Map.ACTOR)
 	if target then
-	--[[	local flash = game.flash.NEUTRAL
+		local flash = game.flash.NEUTRAL
 		if target == game.player then flash = game.flash.BAD end
-		if src == game.player then flash = game.flash.GOOD end]]
+		if src == game.player then flash = game.flash.GOOD end
 
-	--	game.logSeen(target, flash, "%s hits %s for %s%0.2f %s damage#LAST#.", src.name:capitalize(), target.name, DamageType:get(type).text_color or "#aaaaaa#", dam, DamageType:get(type).name)
-		game.logSeen(target, "%s hits %s for %s%0.2f %s damage#LAST#.", src.name:capitalize(), target.name, DamageType:get(type).text_color or "#aaaaaa#", dam, DamageType:get(type).name)
-
+		game.logSeen(target, flash, "%s hits %s for %s%0.2f %s damage#LAST#.", src.name:capitalize(), target.name, DamageType:get(type).text_color or "#aaaaaa#", dam, DamageType:get(type).name)
 		local sx, sy = game.level.map:getTileToScreen(x, y)
 		if target:takeHit(dam, src) then
 			if src == game.player or target == game.player then
