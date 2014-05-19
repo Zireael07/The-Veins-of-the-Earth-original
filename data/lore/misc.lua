33648ac2b95e98439919ab05a3a8b265cb43a13b
Zireael07 <zi@mail.com>
2014-05-19 14:23:28 +0200

    start of a sophisticated text generator for the lore

--- data/lore/misc.lua
+++ data/lore/misc.lua
@@ -1,22 +1,61 @@
+--This is where the text generator lives
+monsters = {"kobolds", "drow", "goblins"}
+possibility = {"can", "may", "might"}
+
+local monster = rng.table(monsters)
+
+kobold_abilities = { "set traps", "hide in shadows", "pepper you with projectiles"}
+drow_abilities = { "envelop you in darkness", "hide in shadows", "use deadly toxins", "set ambushes"}
+goblin_abilities = { "club you to death", "flee in fear"}
+
+local function ability(monster)
+	if not monster then return end
+
+	if monster == "kobolds" then return rng.table(kobold_abilities) end
+	if monster == "drow" then return rng.table(drow_abilities) end
+	if monster == "goblins" then return rng.table(goblin_abilities) end
+end
+
+
+
+newLore{
+	id = "misc-1",
+	category = "misc",
+	name = "Beware",
+	lore = "Beware the "..monster.."! They "..rng.table(possibility).." "..ability(monster)
+}
+
+
+
+
+name = {"Vierna", "Zaknafein", "Drizzt", "Alak", "Nalfein", "Thorik", "Roryn", "Belmara", "Gothog", "Harl", "Talindra", "Alea", "Aravilar", "Rhistel", "Iliphar"}
+
 newLore{
 	id = "misc-2",
 	category = "misc",
-	name = "I was here",
-	lore = [[I was here]],
+	name = "... was here",
+	lore = rng.table(name).." was here..",
+}
+
+--Informative background stuff
+newLore{
+	id = "misc-3",
+	category = "misc",
+	name = "Coins",
+	lore = [[Many drow, elven, dwarven and duergar Houses mint their own coins. However, humans and other races generally use unified coinage.]],
 }
 
 newLore{
-	id = "misc-3",
+	id = "misc-4",
 	category = "misc",
 	name = "A Note on Names",
 	lore = [[Many older names end in -zarr, for example, Symylazarr, Ansrivarr or Ignizarr. Such names are most common among the most-longlived races, that is, elves and drow. Even though, some half-elves bearing such names were seen. ]],
 }
 
-name = {"Vierna", "Zaknafein", "Drizzt", "Alak", "Nalfein", "Thorik", "Roryn", "Belmara", "Gothog", "Harl", "Talindra", "Alea", "Aravilar", "Rhistel", "Iliphar"}
-
+--Test stuff
 newLore{
-	id = "misc-1",
+	id = "misc-5",
 	category = "misc",
-	name = "... was here",
-	lore = rng.table(name).." was here..",
+	name = "I was here",
+	lore = [[I was here]],
 }
