f2045b72ea54da116d1791384a7e972bd9fa6e24
Zireael07 <zi@mail.com>
2014-06-14 17:22:15 +0200

    implement equipdoll in inventory screen

--- dialogs/ShowEquipInven.lua
+++ dialogs/ShowEquipInven.lua
@@ -1,5 +1,5 @@
 -- Veins of the Earth
--- Copyright (C) 2013 Zireael
+-- Copyright (C) 2014 Zireael
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -16,36 +16,56 @@
 
 
 require "engine.class"
+local Base = require "engine.ui.Base"
 local Dialog = require "engine.ui.Dialog"
-local ListColumns = require "engine.ui.ListColumns"
-local Textzone = require "engine.ui.Textzone"
-local TextzoneList = require "engine.ui.TextzoneList"
+local Inventory = require "engine.ui.Inventory"
 local Separator = require "engine.ui.Separator"
-local Button = require "engine.ui.Button"
+local EquipDoll = require "engine.ui.EquipDoll"
+local Tab = require "engine.ui.Tab"
+local ListColumns = require "engine.ui.ListColumns"
 
 module(..., package.seeall, class.inherit(Dialog))
 
-function _M:init(title, actor, filter, action, on_select)
+function _M:init(title, equip_actor, filter, action, on_select, inven_actor)
 	self.action = action
 	self.filter = filter
-	self.actor = actor
+	inven_actor = inven_actor or equip_actor
+	self.equip_actor = equip_actor
+	self.inven_actor = inven_actor
+
 	game.tooltip.add_map_str = nil
 
 	Dialog.init(self, title or "Inventory", math.max(800, game.w * 0.8), math.max(600, game.h * 0.8))
 
-	self.max_h = 0
-
+	
 	-- Add tooltips
 	self.on_select = function(item)
 			if item.last_display_x and item.object then
 			local x
-			game:tooltipDisplayAtMap(game.w*0.2, game.h*0.7, item.object:getDesc({do_color=true}, self.actor:getInven(item.object:wornInven())))
+			game:tooltipDisplayAtMap(game.w*0.2, game.h*0.7, item.object:getDesc({do_color=true}, self.inven_actor:getInven(item.object:wornInven())))
 		elseif item.last_display_x and item.data and item.data.desc then
 			game:tooltipDisplayAtMap(game.w*0.2, game.h*0.7, item.data.desc, {up=true})
 		end
 	end
 
-	self.c_manager = Button.new{text="Item Manager", fct=function() self:onManager() end}
+	self.c_doll = EquipDoll.new{actor=equip_actor, drag_enable=true, filter=filter,
+		fct = function(item, button, event) self:use(item, button, event) end,
+		on_select = function(ui, inven, item, o) if ui.ui.last_display_x then self:select{last_display_x=ui.ui.last_display_x+ui.ui.w, last_display_y=ui.ui.last_display_y, object=o} end end,
+		actorWear = function(ui, wear_inven, wear_item, wear_o)
+			if ui:getItem() then 
+				local bi = self.equip_actor:getInven(ui.inven)
+				local ws = self.equip_actor:getInven(wear_o:wornInven())
+				local os = self.equip_actor:getObjectOffslot(wear_o)
+				if bi and ((ws and ws.id == bi.id) or (os and self.equip_actor:getInven(os).id == bi.id)) then
+					self.equip_actor:doTakeoff(ui.inven, ui.item, ui:getItem(), true, self.inven_actor)
+				end
+			end
+			self.equip_actor:doWear(wear_inven, wear_item, wear_o, self.inven_actor)
+			self.c_inven:generateList()
+		end
+	}
+
+	self.max_h = 0
 
 	self.c_inven = ListColumns.new{width=math.floor(self.iw / 2 - 10), height=self.ih - self.max_h*self.font_h - 10, sortable=true, scrollbar=true, columns={
 		{name="", width={25,"fixed"}, display_prop="char", sort="id"},
@@ -55,82 +75,100 @@ function _M:init(title, actor, filter, action, on_select)
 		{name="Enc.", width=10, display_prop="encumberance", sort="encumberance"},
 	}, list={}, fct=function(item, sel, button, event) self:use(item, button, event) end, select=function(item, sel) self:select(item) end, on_drag=function(item) if self.on_drag then self.on_drag(item) end end}
 
-	self.c_equip = ListColumns.new{width=math.floor(self.iw / 2 - 10), height=self.ih - self.max_h*self.font_h - 10, scrollbar=true, columns={
-		{name="", width={25,"fixed"}, display_prop="char"},
-		{name="", width={8+16,"fixed"}, display_prop="object", direct_draw=function(item, x, y) if item.object then item.object:toScreen(nil, x+4, y, 16, 16) end end},
-		{name="Equipment", width=72, display_prop="name"},
-		{name="Category", width=20, display_prop="cat"},
-		{name="Enc.", width=10, display_prop="encumberance"},
-	}, list={}, fct=function(item, sel, button, event) self:use(item, button, event) end, select=function(item, sel) self:select(item) end, on_drag=function(item) if self.on_drag then self.on_drag(item) end end}
-
 	self:generateList()
+--[[	self.c_inven = Inventory.new{actor=inven_actor, inven=inven_actor:getInven("INVEN"), width=self.iw - 20 - self.c_doll.w, height=self.ih - 10, filter=filter,
+		default_last_tabs = "all",
+		fct=function(item, sel, button, event) self:use(item, button, event) end,
+		select=function(item, sel) self:select(item) end,
+		select_tab=function(item) self:select(item) end,
+		on_drag=function(item) self:onDrag(item) end,
+		on_drag_end=function() self:onDragTakeoff() end,
+		special_bg=function(item) if item.object and item.object.__transmo then return colors.GOLD end end,
+	}]]
+	
+--	self.c_inven.c_inven.on_focus_change = function(ui_self, status) if status == true then self:select(ui_self.list[ui_self.sel], true) end end
 
-	self:loadUI{
-		{left=0, top=0, ui=self.c_equip},
+	local uis = {
+		{left=0, top=0, ui=self.c_doll},
 		{right=0, top=0, ui=self.c_inven},
-		{hcenter=0, top=5, ui=Separator.new{dir="horizontal", size=self.ih - 50}},
-		{left=0, bottom=0, ui=self.c_manager},
-		{left=0, bottom=self.c_manager.h, ui=Separator.new{dir="vertical", size=self.iw - 10}},
-		
+		{left=self.c_doll.w, top=5, ui=Separator.new{dir="horizontal", size=self.ih - 10}},
 	}
+
+--	self:triggerHook{"EquipInvenDialog:makeUI", uis=uis}
+
+	self:loadUI(uis)
 	self:setFocus(self.c_inven)
 	self:setupUI()
 
+	
+	self.key:reset()
+	engine.interface.PlayerHotkeys:bindAllHotkeys(self.key, function(i) self:defineHotkey(i) end)
+	self.key.any_key = function(sym)
+		-- Control resets the tooltip
+		if sym == self.key._LCTRL or sym == self.key._RCTRL then 
+			local ctrl = core.key.modState("ctrl")
+			if self.prev_ctrl ~= ctrl then self:select(self.cur_item, true) end
+			self.prev_ctrl = ctrl
+		end
+	end
+	
 	self.key:addCommands{
-		__TEXTINPUT = function(c)
-			local list
-			if self.focus_ui and self.focus_ui.ui == self.c_inven then list = self.c_inven.list
-			elseif self.focus_ui and self.focus_ui.ui == self.c_equip then list = self.c_equip.list
-			end
-			if list and list.chars[c] then
-				self:use(list[list.chars[c]])
-			end
-		end,
+		[{"_TAB","shift"}] = function() self:moveFocus(1) end,
 	}
-	if engine.interface and engine.interface.PlayerHotkeys then engine.interface.PlayerHotkeys:bindAllHotkeys(self.key, function(i) self:defineHotkey(i) end) end
 	self.key:addBinds{
-		ACCEPT = function()
-			if self.focus_ui and self.focus_ui.ui == self.c_inven then self:use(self.c_inven.list[self.c_inven.sel])
-			elseif self.focus_ui and self.focus_ui.ui == self.c_equip then self:use(self.c_equip.list[self.c_equip.sel])
-			end
+		ACCEPT = function() if self.focus_ui and self.focus_ui.ui == self.c_inven then self:use(self.c_inven.c_inven.list[self.c_inven.c_inven.sel]) end end,
+		EXIT = function()
+		--[[	if self.c_inven.c_inven.scrollbar then
+				self.equip_actor.inv_scroll = self.c_inven.c_inven.scrollbar.pos or 0
+			end]]
+			game.tooltip.locked = false
+			game:unregisterDialog(self)
 		end,
-		EXIT = function() game:unregisterDialog(self) end,
+		MOVE_UP = function() game.log("up") if game.tooltip.locked then game.tooltip.container.scroll_inertia = math.min(game.tooltip.container.scroll_inertia, 0) - 5 end end,
+		MOVE_DOWN = function() if game.tooltip.locked then game.tooltip.container.scroll_inertia = math.max(game.tooltip.container.scroll_inertia, 0) + 5 end end,
+		LOCK_TOOLTIP = lock_tooltip,
+		LOCK_TOOLTIP_COMPARE = lock_tooltip,
+		SCREENSHOT = function() if type(game) == "table" and game.key then game.key:triggerVirtual("SCREENSHOT") end end,
 	}
+	if self.equip_actor.inv_scroll and self.c_inven.c_inven.scrollbar then
+		self.c_inven.c_inven.scrollbar.pos = util.bound(self.equip_actor.inv_scroll, 0, self.c_inven.c_inven.scrollbar.max)
+	end
 end
+
+--[[function _M:firstDisplay()
+	self.cur_item = nil
+	self.c_inven.c_inven:onSelect(true)
+end]]
 
 function _M:on_register()
 	game:onTickEnd(function() self.key:unicodeInput(true) end)
 end
 
 function _M:defineHotkey(id)
-	if not self.actor or not self.actor.hotkey then return end
+	if self.equip_actor ~= self.inven_actor then return end
+	if not self.equip_actor or not self.equip_actor.hotkey then return end
 
 	local item = nil
-	if self.focus_ui and self.focus_ui.ui == self.c_inven then item = self.c_inven.list[self.c_inven.sel]
-	elseif self.focus_ui and self.focus_ui.ui == self.c_equip then item = self.c_equip.list[self.c_equip.sel]
+	if self.focus_ui and self.focus_ui.ui == self.c_inven then item = self.c_inven.c_inven.list[self.c_inven.c_inven.sel]
+	elseif self.focus_ui and self.focus_ui.ui == self.c_doll then item = {object=self.c_doll:getItem()}
 	end
 	if not item or not item.object then return end
 
-	self.actor.hotkey[id] = {"inventory", item.object:getName{no_add_name=true, no_count=true}}
+	self.equip_actor.hotkey[id] = {"inventory", item.object:getName{no_add_name=true, no_count=true}}
 	self:simplePopup("Hotkey "..id.." assigned", item.object:getName{no_add_name=true, no_count=true}:capitalize().." assigned to hotkey "..id)
-	self.actor.changed = true
+	self.equip_actor.changed = true
 end
 
-function _M:select(item)
-	if self.cur_item == item then return end
-	if item then
-		if self.on_select then self.on_select(item) end
-	end
+function _M:select(item, force)
+	--if self.cur_item == item and not force then return end
+	if item then self.on_select(item) end
 	self.cur_item = item
 end
 
-function _M:on_focus(id, ui)
-	if self.focus_ui and self.focus_ui.ui == self.c_inven then self:select(self.c_inven.list[self.c_inven.sel])
-	elseif self.focus_ui and self.focus_ui.ui == self.c_equip then self:select(self.c_equip.list[self.c_equip.sel])
-	end
-end
-
 function _M:use(item, button, event)
+--[[	if self.c_inven.c_inven.scrollbar then
+		self.equip_actor.inv_scroll = self.c_inven.c_inven.scrollbar.pos or 0
+	end]]
 	if item then
 		if self.action(item.object, item.inven, item.item, button, event) then
 			game:unregisterDialog(self)
@@ -138,48 +176,74 @@ function _M:use(item, button, event)
 	end
 end
 
-function _M:generateList(no_update)
-	-- Makes up the list
-	self.equip_list = {}
-	local list = self.equip_list
-	local chars = {}
-	local i = 1
-	self.max_h = 0
-	for inven_id =  1, #self.actor.inven_def do
-		if self.actor.inven[inven_id] and (self.actor.inven_def[inven_id].is_worn or self.actor.inven_def[inven_id].is_shown_equip) then
-			list[#list+1] = { id=#list+1, char="", name=tstring{{"font", "bold"}, self.actor.inven_def[inven_id].name, {"font", "normal"}}, color={0x90, 0x90, 0x90}, inven=inven_id, cat="", encumberance="", desc=self.actor.inven_def[inven_id].description }
-			self.max_h = math.max(self.max_h, #self.actor.inven_def[inven_id].description:splitLines(self.iw - 10, self.font))
+function _M:unload()
+	for inven_id = 1, #self.inven_actor.inven_def do if self.inven_actor.inven[inven_id] then for item, o in ipairs(self.inven_actor.inven[inven_id]) do o.__new_pickup = nil end end end
+end
 
-			for item, o in ipairs(self.actor.inven[inven_id]) do
-				if not self.filter or self.filter(o) then
-					local char = self:makeKeyChar(i)
+function _M:updateTitle(title)
+	Dialog.updateTitle(self, title)
 
-					local enc = 0
-					o:forAllStack(function(o) enc=enc+o.encumber end)
+	local green = colors.LIGHT_GREEN
+	local red = colors.LIGHT_RED
 
-					list[#list+1] = { id=#list+1, char=char, name=o:getName(), sortname=o:getName():toString():removeColorCodes(), color=o:getDisplayColor(), object=o, inven=inven_id, item=item, cat=o.subtype, encumberance=enc, desc=o:getDesc() }
-					chars[char] = #list
-					i = i + 1
-				end
-			end
-		end
+	local enc, max = self.equip_actor:getEncumbrance(), self.equip_actor:getMaxEncumbrance()
+	local v = math.min(enc, max) / max
+	self.title_fill = self.iw * v
+	self.title_fill_color = {
+		r = util.lerp(green.r, red.r, v),
+		g = util.lerp(green.g, red.g, v),
+		b = util.lerp(green.b, red.b, v),
+	}
+	if self.equip_actor.inv_scroll and self.c_inven.c_inven.scrollbar then
+		self.c_inven.c_inven.scrollbar.pos = util.bound(self.equip_actor.inv_scroll, 0, self.c_inven.c_inven.scrollbar.max)
 	end
-	list.chars = chars
-	self.equip_list = list
+end
 
+function _M:onDrag(item)
+	if item and item.object then
+		local s = item.object:getEntityFinalSurface(nil, 64, 64)
+		local x, y = core.mouse.get()
+		game.mouse:startDrag(x, y, s, {kind="inventory", item_idx=item.item, inven=item.inven, object=item.object, id=item.object:getName{no_add_name=true, force_id=true, no_count=true}}, function(drag, used)
+			if not used then
+				local x, y = core.mouse.get()
+				game.mouse:receiveMouse("drag-end", x, y, true, nil, {drag=drag})
+			end
+		end)
+	end
+end
+
+function _M:onDragTakeoff()
+	local drag = game.mouse.dragged.payload
+	if drag.kind == "inventory" and drag.inven and self.equip_actor:getInven(drag.inven) and self.equip_actor:getInven(drag.inven).worn then
+		self.equip_actor:doTakeoff(drag.inven, drag.item_idx, drag.object, nil, self.inven_actor)
+		self:generateList()
+		game.mouse:usedDrag()
+	end
+end
+
+function _M:drawFrame(x, y, r, g, b, a)
+	Dialog.drawFrame(self, x, y, r, g, b, a)
+	if r == 0 then return end -- Drawing the shadow
+	if self.ui ~= "metal" then return end
+	if not self.title_fill then return end
+
+	core.display.drawQuad(x + self.frame.title_x, y + self.frame.title_y, self.title_fill, self.frame.title_h, self.title_fill_color.r, self.title_fill_color.g, self.title_fill_color.b, 60)
+end
+
+function _M:generateList(no_update)
 	-- Makes up the list
 	self.inven_list = {}
 	local list = self.inven_list
 	local chars = {}
 	local i = 1
-	for item, o in ipairs(self.actor:getInven("INVEN") or {}) do
+	for item, o in ipairs(self.inven_actor:getInven("INVEN") or {}) do
 		if not self.filter or self.filter(o) then
 			local char = self:makeKeyChar(i)
 
 			local enc = 0
 			o:forAllStack(function(o) enc=enc+o.encumber end)
 
-			list[#list+1] = { id=#list+1, char=char, name=o:getName(), sortname=o:getName():toString():removeColorCodes(), color=o:getDisplayColor(), object=o, inven=self.actor.INVEN_INVEN, item=item, cat=o.subtype, encumberance=enc, desc=o:getDesc() }
+			list[#list+1] = { id=#list+1, char=char, name=o:getName(), sortname=o:getName():toString():removeColorCodes(), color=o:getDisplayColor(), object=o, inven=self.inven_actor.INVEN_INVEN, item=item, cat=o.subtype, encumberance=enc, desc=o:getDesc() }
 			chars[char] = #list
 			i = i + 1
 		end
@@ -188,18 +252,6 @@ function _M:generateList(no_update)
 
 	if not no_update then
 		self.c_inven:setList(self.inven_list)
-		self.c_equip:setList(self.equip_list)
+	--	self.c_equip:setList(self.equip_list)
 	end
 end
-
-function _M:on_recover_focus()
-	self:generateList()
-end
-
-function _M:onManager()
-	game:unregisterDialog(self)
-    game:registerDialog(require("mod.dialogs.ItemManager").new(game.player))
-
-end
-
-
--- load.lua
+++ load.lua
@@ -130,7 +130,7 @@ ActorTalents:loadDefinition("/data/talents.lua")
 ActorTemporaryEffects:loadDefinition("/data/timed_effects.lua")
 
 -- Actor inventory
-ActorInventory:defineInventory('MAIN_HAND', 'Main hand', true, 'Weapon')
+--[[ActorInventory:defineInventory('MAIN_HAND', 'Main hand', true, 'Weapon')
 ActorInventory:defineInventory('OFF_HAND', 'Off hand', true, 'Shield')
 ActorInventory:defineInventory('BODY', 'Armor', true, 'Body armor')
 ActorInventory:defineInventory('CLOAK', 'Cloak', true, 'Cloak')
@@ -142,7 +142,41 @@ ActorInventory:defineInventory('HELM', 'Head', true, 'Helmets or other headwear'
 ActorInventory:defineInventory('RING', 'Fingers', true, 'Rings')
 ActorInventory:defineInventory('AMULET', 'Neck', true, 'Amulet')
 ActorInventory:defineInventory('LITE', 'Light', true, 'Torches or lantern')
-ActorInventory:defineInventory('TOOL', 'Tool', true, 'Shovel or other tool')
+ActorInventory:defineInventory('TOOL', 'Tool', true, 'Shovel or other tool')]]
+
+ActorInventory:defineInventory("MAIN_HAND", "In main hand", true, "Most weapons are wielded in the main hand.", nil, {equipdoll_back="ui/equipdoll/mainhand_inv.png"})
+ActorInventory:defineInventory("OFF_HAND", "In off hand", true, "You can use shields or a second weapon in your off-hand, if you have the talents for it.", nil, {equipdoll_back="ui/equipdoll/offhand_inv.png"})
+ActorInventory:defineInventory("BODY", "Main armor", true, "Armor protects you from physical attacks. The heavier the armor the more it hinders the use of talents and spells.", nil, {equipdoll_back="ui/equipdoll/armor_inv.png"})
+ActorInventory:defineInventory("CLOAK", "Cloak", true, "A cloak can simply keep you warm or grant you wondrous powers should you find a magical one.", nil, {equipdoll_back="ui/equipdoll/cloak_inv.png"})
+ActorInventory:defineInventory("BELT", "Around waist", true, "Belts are worn around your waist.", nil, {equipdoll_back="ui/equipdoll/belt_inv.png"})
+ActorInventory:defineInventory("QUIVER", "Quiver", true, "Your readied ammo.", nil, {equipdoll_back="ui/equipdoll/ammo_inv.png"})
+ActorInventory:defineInventory("GLOVES", "On hands", true, "Various gloves can be worn on your hands.", nil, {equipdoll_back="ui/equipdoll/ring_inv.png"})
+ActorInventory:defineInventory("BOOTS", "On feet", true, "Sandals or boots can be worn on your feet.", nil, {equipdoll_back="ui/equipdoll/boots_inv.png"})
+ActorInventory:defineInventory("HELM", "On head", true, "You can wear helmets or crowns on your head.", nil, {equipdoll_back="ui/equipdoll/head_inv.png"})
+ActorInventory:defineInventory("RING", "On fingers", true, "Rings are worn on fingers.", nil, {equipdoll_back="ui/equipdoll/ring_inv.png"})
+ActorInventory:defineInventory("AMULET", "Around neck", true, "Amulets are worn around the neck.", nil, {equipdoll_back="ui/equipdoll/amulet_inv.png"})
+ActorInventory:defineInventory("LITE", "Light source", true, "A light source allows you to see in the dark places of the world.", nil, {equipdoll_back="ui/equipdoll/light_inv.png"})
+ActorInventory:defineInventory("TOOL", "Tool", true, "This is your readied tool, usually a shovel.", nil, {equipdoll_back="ui/equipdoll/tool_inv.png"})
+
+ActorInventory.equipdolls = {
+	default = { w=48, h=48, itemframe="ui/equipdoll/itemframe48.png", itemframe_sel="ui/equipdoll/itemframe-sel48.png", ix=3, iy=3, iw=42, ih=42, doll_x=116, doll_y=168+64, list={
+		MAIN_HAND = {{weight=2, x=48, y=120}},
+		OFF_HAND = {{weight=3, x=48, y=192}},
+		BODY = {{weight=4, x=48, y=264}},
+		CLOAK = {{weight=13, x=264, y=120}},
+		BELT = {{weight=11, x=264, y=264}},
+		QUIVER = {{weight=5, x=48, y=336}},
+		GLOVES = {{weight=12, x=264, y=192}},
+		BOOTS = {{weight=10, x=264, y=336}},
+		HELM = {{weight=15, x=120, y=48, text="topleft"}},
+		RING = {{weight=6, x=48, y=408}, {weight=7, x=120, y=408, text="bottom"}},
+		AMULET = {{weight=14, x=192, y=48, text="topright"}},
+		LITE = {{weight=8, x=192, y=408}},
+		TOOL = {{weight=9, x=264, y=408, text="bottom"}},
+	}},
+}
+
+
 
 -- Actor stats
 ActorStats:defineStat("Strength",	"str", 0, -5, 30, "Strength measures your character’s muscle and physical power.")
