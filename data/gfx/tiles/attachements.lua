960f386bd1518d50983bfed2df2752760dfda2c2
Zireael07 <zi@mail.com>
2014-06-20 13:32:58 +0200

    define attachments

---
+++ data/gfx/tiles/attachements.lua
@@ -0,0 +1,30 @@
+tiles={} dolls={}
+
+tiles["player/human_m.png"] = { base=32,
+	feet = {x=11, y=27},
+	hand1 = {x=17, y=6},
+	hand2 = {x=17, y=7},
+	belly = {x=16, y=14},
+	back = {x=9, y=15},
+	head = {x=5, y=15},
+}
+
+tiles["player.png"] = { base=32,
+	feet = {x=11, y=27},
+	hand1 = {x=17, y=6},
+	hand2 = {x=17, y=7},
+	belly = {x=16, y=14},
+	back = {x=9, y=15},
+	head = {x=5, y=15},
+}
+
+
+dolls.race_runic_golem = dolls.race_runic_golem or {}
+dolls.race_runic_golem.all = { base=32,
+	feet = {x=11, y=27},
+	hand1 = {x=17, y=6},
+	hand2 = {x=17, y=7},
+	belly = {x=16, y=14},
+	back = {x=9, y=15},
+	head = {x=5, y=15},
+}
