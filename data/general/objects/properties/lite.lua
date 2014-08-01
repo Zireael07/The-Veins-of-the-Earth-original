83021d176bf2385693058d481e4efd868aab30e4
Zireael07 <zi@mail.com>
2014-08-01 16:39:56 +0200

    add egos for lites

---
+++ data/general/objects/properties/lite.lua
@@ -0,0 +1,17 @@
+--Veins of the Earth
+--Zireael 2014
+
+newEntity {
+	name = " of brightness", suffix = true,
+--	keywords = {bonus=true},
+	level_range = {1, 10},
+	rarity = 5,
+	cost = 2000,
+	wielder = {
+		lite = 1,
+	},
+}
+
+--TO DO: of telepathy, of vision (see invisible),
+
+--TO DO curses: of peril (0 lite, aggravate); 
