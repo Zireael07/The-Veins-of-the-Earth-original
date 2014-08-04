2a07481e075deee9bb57e7f45c9270a184728122
Zireael07 <zi@mail.com>
2014-08-04 15:21:37 +0200

    tweak the feat selection screens so that you can't click on learned feats

--- dialogs/BonusFeatDialog.lua
+++ dialogs/BonusFeatDialog.lua
@@ -33,22 +33,29 @@ function _M:init(actor)
 	self:generateLists()
 	
 	self.c_points = Textzone.new{width=self.iw, height = 50, text = "Available fighter bonus feat points: "..self.player.fighter_bonus}	
-	self.c_learned = List.new{width=250, nb_items=#self.list_learned, height = self.ih*0.8, list=self.list_learned, fct=function(item) self:use(item) end, select=function(item,sel) self:on_select(item,sel) end, scrollbar=true}
+	self.c_learned_text = Textzone.new{auto_width=true, auto_height=true, text="#SANDY_BROWN#Learned feats#LAST#"}
+	self.c_learned = SurfaceZone.new{width=250, height=self.ih*0.8,alpha=1.0}
+	self.c_avail_text = Textzone.new{auto_width=true, auto_height=true, text="#SANDY_BROWN#Available feats#LAST#"}
 	self.c_avail = List.new{width=250, nb_items=#self.list_avail, height = self.ih*0.8, list=self.list_avail, fct=function(item) self:use(item) end, select=function(item,sel) self:on_select(item,sel) end, scrollbar=true}
+	self.c_barred_text = Textzone.new{auto_width=true, auto_height=true, text="#SANDY_BROWN#Barred feats#LAST#"}
 	self.c_barred = List.new{width=250, nb_items=#self.list_barred, height = self.ih*0.7, list=self.list_barred, fct=function(item) self:use(item) end, select=function(item,sel) self:on_select(item,sel) end, scrollbar=true}
 	self.c_desc = TextzoneList.new{width=self.iw/4-20, height = 400, text="Hello from description"}
 
 
 	self:loadUI{
 		{left=0, top=0, ui = self.c_points},
-		{left=0, top=50, ui=self.c_learned},
-		{left=250, top=50, ui=self.c_avail},
-		{left=500, top=50, ui=self.c_barred},
-		{left=750, top=0, ui=self.c_desc}
+		{left=0, top=90, ui=self.c_learned_text},
+		{left=250, top=90, ui=self.c_avail_text},
+		{left=500, top=90, ui=self.c_barred_text},
+		{left=0, top=self.c_points.h + 5 + self.c_bonus.h + 5 + self.c_learned_text.h + 15, ui=self.c_learned},
+		{left=250, top=self.c_points.h + 5 + self.c_bonus.h + 5 + self.c_learned_text.h + 15, ui=self.c_avail},
+		{left=500, top=self.c_points.h + 5 + self.c_bonus.h + 5 + self.c_learned_text.h + 15, ui=self.c_barred},
+		{left=750, top=self.c_points.h + 5 + self.c_bonus.h + 5 + self.c_learned_text.h + 15, ui=self.c_desc}
 	}
 	self:setFocus(self.c_avail)
 --	self:setupUI(false, true)
 	self:setupUI()
+	self:drawDialog()
 
 --	self.key:addBind("EXIT", function() game:unregisterDialog(self) end)
 
@@ -108,56 +115,20 @@ end
 
 function _M:update()
 	local sel = self.selection
---	game.log(""..self.selection)
 	self:generateLists() -- Slow! Should just update the one changed and sort again
 	self.c_points.text = "Available fighter bonus feat points: "..self.player.fighter_bonus
 	self.c_points:generate()
-	self.c_learned.list = self.list_learned
 	self.c_avail.list = self.list_avail
 	self.c_barred.list = self.list_barred
-	self.c_learned:generate()
 	self.c_avail:generate()
 	self.c_barred:generate()
+	self:drawDialog()
 --	if sel then self.c_list:select(sel) end
 end
 
 function _M:generateLists()
-	self:generateLearned()
 	self:generateAvail()
 	self:generateBarred()
-end
-
-
-function _M:generateLearned()
-	local player = game.player
-    local list = {}
-    for tid, _ in pairs(player.talents_def) do
-		local t = player:getTalentFromId(tid)
-        if t.is_feat and t.fighter and player:knowTalent(t) then
-        	--if we haven't learned it, and it is a class feat, dont show it in feat list
-        	local tt = player:getTalentTypeFrom(t.type[1])
-        	if not tt.passive or player:knowTalent(t) then
-	        	local tid
-	        	local color
-	        	if player:knowTalent(t) then color = {255,255,255} i = 1
-	     		elseif player:canLearnTalent(t) then color = {0,187,187} i = 2
-	     		else color = {100, 100, 100} i = 3
-	     		end
-	     		local d = "#GOLD#"..t.name.."#LAST#\n"
-	     		s = player:getTalentReqDesc(t.id):toString()
-	     		d = d..s.."\n#WHITE#"
-	     		if t.acquired then d = d.."#PINK#"..t.acquired.."#LAST#\n\n" end
-	     		d = d..t.info(player,t)
-	            list[#list+1] = {name=t.name, color = color, desc=d, talent=t, i = i}
-	        end
-        end
-    end
-    self.list_learned = list
-    --Sort it alphabetically
-    table.sort(self.list_learned, function (a,b) 
-    		return a.name < b.name
-    end)
-
 end
 
 function _M:generateAvail()
@@ -222,4 +193,65 @@ function _M:generateBarred()
      		return a.name < b.name
     end)
 
+end
+
+function _M:mouseZones(t, no_new)
+    -- Offset the x and y with the window position and window title
+    if not t.norestrict then
+        for i, z in ipairs(t) do
+            if not z.norestrict then
+                z.x = z.x + self.display_x + 5
+                z.y = z.y + self.display_y + 20 + 3
+            end
+        end
+    end
+
+    if not no_new then self.mouse = engine.Mouse.new() end
+    self.mouse:registerZones(t)
+end
+
+function _M:mouseTooltip(text, _, _, _, w, h, x, y)
+    self:mouseZones({
+        { x=x, y=y, w=w, h=h, fct=function(button) game.tooltip_x, game.tooltip_y = 100, 100; game:tooltipDisplayAtMap(game.w*0.8, game.h*0.8, text) end},
+    }, true)
+end
+
+function _M:drawDialog()
+	local player = self.actor
+    local s = self.c_learned.s
+
+    s:erase(0,0,0,0)
+
+    local h = 0
+    local w = 0
+
+    h = 0
+    w = 0
+
+    local list = {}
+  
+        for j, t in pairs(player.talents_def) do
+          if player:knowTalent(t.id) and t.is_feat then
+                if player:classFeat(t.id) then name = "#GOLD#"..t.name
+                else name = ("%s"):format(t.name) end
+
+
+                list[#list+1] = {
+                    name = name,
+                --    name = ("%s"):format(t.name),
+                --    desc = player:getTalentFullDescription(t):toString(),
+                    desc = ("%s"):format(t.info(player,t)),
+                }
+            end
+        end
+
+        table.sort(list, function(a,b) return a.name < b.name end)
+
+        for i, t in ipairs(list) do
+        --    self:mouseTooltip(t.desc, s:drawColorStringBlended(self.font, ("%s"):format(t.name), w, h, 255, 255, 255, true)) h = h + self.font_h
+        	s:drawColorStringBlended(self.font, ("%s"):format(t.name), w, h, 255, 255, 255, true) h = h + self.font_h
+        end
+
+    self.c_learned:generate()
+    self.changed = false
 end
