a48566d4dcdf081b759a0566134ead38be466e7d
Zireael07 <zi@mail.com>
2014-08-23 21:04:51 +0200

    hiring now requires diplomacy checks and subtracts money

--- data/chats/hireling.lua
+++ data/chats/hireling.lua
@@ -1,6 +1,8 @@
 --Veins of the Earth
 --Zireael 2014
 
+local cost
+
 newChat{ id="start",
     text = [[Welcome, adventurer! Would you like to have some company?]],
     answers = {
@@ -26,9 +28,14 @@ newChat{ id="company",
 newChat{ id="cost",
     text = [[It costs 3 gp per day.]],
     answers = {
-        {[[Fine. Let's head out.]], jump="hire"},
         {[[That's too costly.]], jump="again"},
-    }
+        {[[Fine. Let's head out.]], action = function(npc, player)
+        if player:skillCheck("diplomacy", 15) then return "hire" end
+        --   cost = 3
+        return "sorry" end,
+        },
+        
+    },
 }
 
 newChat{ id="again",
@@ -38,12 +45,36 @@ newChat{ id="again",
     }
 }
 
+newChat{ id="sorry",
+    text = [[Sorry, I'm not interested. Maybe if you paid me more?]],
+    answers = {
+        {[[What if I paid you double?]], jump="double"},
+        {[[That's too bad. I'll manage on my own.]]},
+    }
+    
+}
+
+newChat{ id="double",
+    text = [[That's a whole different matter. 6 gp per day it is.]],
+    answers = {
+    {[[Great, let's go!]], action = function(npc, player)
+        if player:skillCheck("diplomacy", 10) then 
+        --    cost = 6
+            return "hire2" end
+     return "sorry"  end,
+    },
+    {[[I changed my mind.]], jump="again"},
+}
+}
+
 newChat{ id="hire",
     text = [[Glad that's settled then. Don't forget to pay.]],
     answers = {
         {[[Let's go!]], action = function(npc, player) 
         --That's where the hireling should join the party
         npc.faction = player.faction 
+        --Deduce the gold
+        player.money = player.money - 3
         --Prevent further talks
         npc.can_talk = nil
         end,
@@ -54,8 +85,26 @@ newChat{ id="hire",
     
 }
 
---if npc.faction == player.faction then
+newChat{ id="hire2",
+    text = [[Glad that's settled then. Don't forget to pay.]],
+    answers = {
+        {[[Let's go!]], action = function(npc, player) 
+        --That's where the hireling should join the party
+        npc.faction = player.faction 
+        --Deduce the gold
+        player.money = player.money - 6
+        --Prevent further talks
+        npc.can_talk = nil
+        end,
+
+        }, --cond = function(npc, player) player.level > 6 end
+        {[[I changed my mind.]], jump="again"},
+    }
+    
+}
+
+
 
 return "start"
 
---else end
+
