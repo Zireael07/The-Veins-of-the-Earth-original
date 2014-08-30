fd0be710c2f67a00a42bd5871957e8ad6ec4109c
Zireael07 <zi@mail.com>
2014-08-30 14:33:20 +0200

    new zone with Forest generator

---
+++ data/zones/jungle tunnels/objects.lua
@@ -0,0 +1,174 @@
+-- Veins of the Earth
+-- Zireael
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+local Talents = require "engine.interface.ActorTalents"
+
+local luck = (game.player:getLuc()-10)/2
+
+load("/data/general/objects/armor.lua") -- rarity(-luck))
+load("/data/general/objects/shields.lua")
+
+load("/data/general/objects/weapons.lua") --rarity(-luck))
+load("/data/general/objects/ranged.lua") --rarity(-luck))
+load("/data/general/objects/exotic.lua") --rarity(-luck))
+load("/data/general/objects/exoticranged.lua") --, rarity(-luck))
+load("/data/general/objects/reach.lua") --rarity(-luck))
+load("/data/general/objects/thrown.lua")
+
+load("/data/general/objects/consumables.lua")
+load("/data/general/objects/containers.lua")
+
+load("/data/general/objects/wondrous_items.lua") --, rarity(-luck))
+load("/data/general/objects/magic_items.lua") -- rarity(-luck))
+load("/data/general/objects/poisons.lua")
+
+load("/data/general/objects/pickaxes.lua")
+load("/data/general/objects/money.lua") -- rarity(-luck))
+
+load("/data/general/objects/specific_items.lua")
+
+--Lore
+load("/data/general/objects/lore.lua")
+
+--Lore
+for i = 1, 7 do
+newEntity{ base = "BASE_LORE",
+    define_as = "NOTE"..i,
+    name = "tattered paper scrap", lore="misc-"..i,
+    desc = [[A paper scrap, left by an adventurer.]],
+    rarity = false,
+    encumberance = 0,
+}
+end
+
+
+--Bones
+newEntity{
+    define_as = "BONES",
+    image = "tiles/bones.png",
+    display = "%", color=colors.WHITE,
+    encumber = 30,
+    name = "bones",
+    desc = [[Bones of some dead adventurer.]],
+}
+
+--Light sources
+newEntity{
+    define_as = "BASE_LIGHT",
+    slot = "LITE",
+    type = "torch", subtype = "lite",
+    image = "tiles/torch.png",
+    display = "~", color=colors.YELLOW,
+    encumber = 0,
+    rarity = 10,
+    name = "torch",
+    desc = [[A torch.]],
+}
+
+--Should last 5000 turns
+newEntity{
+    base = "BASE_LIGHT",
+    name = "torch",
+    level_range = {1,10},
+    cost = 0,
+    wielder = {
+    lite=2
+  }, 
+}
+ 
+--Unlimited
+newEntity{
+    base = "BASE_LIGHT",
+    name = "everlasting torch",
+    level_range = {10, nil},
+    cost = 5000,
+    wielder = {
+    lite=2
+  }, 
+}
+
+--Should last 7500 turns
+newEntity{
+    base = "BASE_LIGHT",
+    name = "lantern",
+    image = "tiles/lantern.png",
+    level_range = {5,nil},
+    cost = 7,
+    wielder = {
+    lite=3
+  }, 
+}
+
+--Burnt out torch
+newEntity{
+    base = "BASE_LIGHT",
+    name = "burnt out torch",
+    level_range = {1,10},
+    cost = 0,
+}
+
+--Burnt out lantern
+newEntity{
+    base = "BASE_LIGHT",
+    name = "burnt out lantern",
+    image = "tiles/lantern.png",
+    level_range = {5,nil},
+    cost = 0,
+}
+
+
+--Tools or kits
+newEntity{
+    define_as = "BASE_TOOL",
+    slot = "TOOL",
+    type = "tool", subtype = "skill",
+    image = "tiles/kit.png",
+    display = "^", color=colors.YELLOW,
+    encumber = 1,
+    rarity = 10,
+    name = "tool kit",
+    desc = [[tool kit.]],
+}
+
+newEntity{
+    base = "BASE_TOOL",
+    name = "healing kit",
+    level_range = {1,10},
+    cost = 100,
+    wielder = {
+    skill_bonus_heal = 2
+  }, 
+}
+
+newEntity{
+    base = "BASE_TOOL",
+    name = "lockpicking kit",
+    level_range = {1,10},
+    cost = 100,
+    wielder = {
+    skill_bonus_openlock = 2
+  }, 
+}
+
+newEntity{
+    base = "BASE_TOOL",
+    name = "survival kit",
+    level_range = {1,10},
+    cost = 100,
+    wielder = {
+    skill_bonus_survival = 2
+  }, 
+}
---
+++ data/zones/jungle tunnels/zone.lua
@@ -0,0 +1,86 @@
+-- Veins of the Earth
+-- Zireael 2014
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+return {
+	name = "Jungle Tunnels",
+	level_range = {1, 1},
+	max_level = 1000,
+--	decay = {300, 800},
+	width = 100, height = 100,
+--	persistent = "zone",
+	generator =  {
+		map = {
+			class = "engine.generator.map.Forest",
+			edge_entrances = {4,6},
+			zoom = 4,
+			sqrt_percent = 30,
+			noise = "fbm_perlin",
+		--	floor = function() if rng.chance(20) then return "FLOWER" else return "GRASS" end end,
+			floor = "FLOOR",
+			wall = "WALL",
+			up = {"UP", "SHAFT_UP"},
+			down = { "DOWN", "SHAFT_DOWN" },
+			door = "DOOR",
+		--[[	road = "GRASS_ROAD_DIRT",
+			add_road = true,
+			do_ponds = {
+				nb = {0, 2},
+				size = {w=25, h=25},
+				pond = {{0.6, "DEEP_WATER"}, {0.8, "DEEP_WATER"}},
+			},]]
+
+		--[[	nb_rooms = {0,0,0,1},
+			rooms = {"lesser_vault"},
+			lesser_vaults_list = {"honey_glade", "forest-ruined-building1", "forest-ruined-building2", "forest-ruined-building3", "forest-snake-pit", "mage-hideout"},
+			]]
+			lite_room_chance = 0,
+		},
+		actor = {
+		--	class = "mod.class.generator.actor.EncounterRandom",
+			class = "mod.class.generator.actor.Random",
+			nb_npc = {10, 20},
+		--[[	class = "mod.class.generator.actor.OnSpots",
+				nb_spots = 2, on_spot_chance = 75,]]
+		
+		},
+		object = {
+            class = "engine.generator.object.Random",
+            nb_object = {20, 30},
+        },
+	},
+	levels =
+	{
+	--Place exit to worldmap on level 1
+		[1] = { 
+		generator = { map = { 
+		up = "EXIT",
+		},},
+	},
+	--No shaft up on level 2
+		[2] = { 
+		generator = { map = { 
+		up = "UP",
+		},},
+	},
+
+	},
+
+	post_process = function(level)
+		-- Put lore near the up stairs
+		game:placeRandomLoreObject("NOTE"..level.level)
+
+	end,
+}
