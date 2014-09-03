c7d6e878e1ca42289f7ac5991ed5ea16d7ce0618
Zireael07 <zi@mail.com>
2014-09-03 14:42:00 +0200

    fix loading ticks again

--- init.lua
+++ init.lua
@@ -77,7 +77,7 @@ score_formatters = {
 }
 
 --"Wait finished, counted NNN, MMM ticks." This is NNN
-loading_wait_ticks = 782
+loading_wait_ticks = 100
 
 load_tips = {
   { text = [[If you are a spellcaster, you should prepare your spells in your spellbook and rest ASAP to gain them.]] },
