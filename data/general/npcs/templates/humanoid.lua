04c78f8fe5a5dcdbd0c077f14214f705903ac64e
Zireael07 <zi@mail.com>
2014-09-18 18:41:15 +0200

    add special templates from Incursion to humanoid templates

--- data/general/npcs/templates/humanoid.lua
+++ data/general/npcs/templates/humanoid.lua
@@ -1,6 +1,8 @@
 --Veins of the Earth
 --Zireael
 
+local Talents = require "engine.interface.ActorTalents"
+
 --Damage reduction 5/magic
 newEntity {
 	name = "celestial ", prefix = true, instant_resolve=true,
@@ -144,4 +146,82 @@ newEntity{
 	infravision = 3,
 	challenge = 1,
 	combat = { dam= {1,6} },
+}
+
+
+--Incursion's special templates
+-- +2 Str
+--[[newEntity{
+	name = "bandit ", prefix = true, instant_resolve = true,
+	keywords = {special=true},
+	level_range = {5, nil},
+	rarity = 10,
+
+	max_life = 0,
+	stats = { str = 0, dex = 0, con = 0, int = 0, wis = 0, cha = 0},
+	defense = 0,
+	size = 0,
+	life_rating = 0,
+
+	challenge = 2,
+--[[	resolvers.talents{ [Talents.T_POWER_ATTACK]=1,
+	[Talents.T_MARTIAL_WEAPON_PROFICIENCY]=1,
+	[Talents.T_LIGHT_ARMOR_PROFICIENCY]=1,
+	[Talents.T_MEDIUM_ARMOR_PROFICIENCY]=1,
+	},]]
+--[[}
+
+--+4 Str +4 Con
+newEntity{
+	name = "chieftain ", prefix = true, instant_resolve = true,
+	keywords = {special=true},
+	level_range = {5, nil},
+	rarity = 10,
+
+	max_life = 0,
+	stats = { str = 0, dex = 0, con = 0, int = 0, wis = 0, cha = 0},
+	defense = 0,
+	size = 0,
+	life_rating = 0,
+
+	challenge = 2,
+	combat_attack = 2,
+--	resolvers.talents{ [Talents.T_POWER_ATTACK]=1,
+--	[Talents.T_CLEAVE]=1,
+--	},
+}
+
+--+1 to all attributes
+newEntity{
+	name = "skilled ", prefix = true, instant_resolve = true,
+	keywords = {special=true},
+	level_range = {5, nil},
+	rarity = 10,
+
+	max_life = 0,
+	stats = { str = 0, dex = 0, con = 0, int = 0, wis = 0, cha = 0},
+	defense = 0,
+	size = 0,
+	life_rating = 0,
+
+	hit_die = 1,
+	combat_attack = 1,
+	challenge = 1,
+}
+
+--+2 to all attributes
+newEntity{
+	name = "experienced ", prefix = true, instant_resolve = true,
+	keywords = {special=true},
+	level_range = {5, nil},
+	rarity = 10,
+
+	max_life = 0,
+	stats = { str = 0, dex = 0, con = 0, int = 0, wis = 0, cha = 0},
+	defense = 0,
+	size = 0,
+	life_rating = 0,
+
+	challenge = 2,
+	combat_attack = 2,]]
 }
