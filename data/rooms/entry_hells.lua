301eae80eb19e0f96aefae298025657408099de8
Zireael07 <zi@mail.com>
2014-10-16 14:33:21 +0200

    import two static rooms from Incursion

---
+++ data/rooms/entry_hells.lua
@@ -0,0 +1,16 @@
+--Veins of the Earth
+--Zireael 2014
+
+-- "." bloodstain
+--1 bloodstain w human corpse
+--> : material portal
+
+return {
+[[#######]],
+[[#11111#]],
+[[#11111#]],
+[[#11>11#]],
+[[#11111#]],
+[[#11111#]],
+[[#######]],
+}
---
+++ data/rooms/starting_room.lua
@@ -0,0 +1,51 @@
+--Veins of the Earth
+--Zireael 2014
+
+--23 x 23
+
+--o: pillar
+--b: bloodstain
+--p: potion of healing
+--d: potion of dimension door
+--i: potion of see invisible
+--1: bloodstain w/ human corpse
+--2: bloodstain w/ elf corpse
+--3: bloodstain w/ lizardman corpse
+--4: bloodstain w/ dwarf corpse
+--5: bloodstain w/ gnome corpse
+--6: bloodstain w/ cave orc corpse
+--7: bloodstain w/ kobold corpse
+
+--Desc: "Your first sight of the goblin caves is not a heartening one:
+--this room is a mass grave, the floor splattered with blood and the
+--corpses of your fellow adventurers laying strewn around the stairs.
+--There is an old, thick pillar in each corner, covered with the
+--twisting, indecipherable goblin script; while you can't make out the
+--message, you can discern the runes for 'light' and 'surface'."
+
+
+return {
+[[#######################]],
+[[#pb........d..2......p#]],
+[[#.o......1..b....o..o.#]],
+[[#.....................#]],
+[[#....6...b....b...3...#]],
+[[#..b................o.#]],
+[[#...5................i#]],
+[[#.......b..4.b........#]],
+[[#..3..4...5...........#]],
+[[#.........6...7.b..7..#]],
+[[#..2...1..............#]],
+[[#p.........>.........d#]],
+[[#............3...4....#]],
+[[#...b...........b.....#]],
+[[#.........2...........#]],
+[[#.................b..b#]],
+[[#.............b.b1.b.b#]],
+[[#.o..b....b...b..b.bbb#]],
+[[#............b.b...b5b#]],
+[[#.............bbb.b.bb#]],
+[[#.o..o7....bb.b6bb.bob#]],
+[[#d.........pbbbb..bb.p#]],
+[[#######################]],
+}
