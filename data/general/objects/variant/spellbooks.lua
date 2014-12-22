7e67265403bcee009206f232157a64bfad7a93e4
Zireael07 <zi@mail.com>
2014-12-22 18:57:49 +0100

    begin work on holy symbols & spellbooks variant file

---
+++ data/general/objects/variant/spellbooks.lua
@@ -0,0 +1,109 @@
+--Veins of the Earth
+--Zireael 2014
+
+newEntity{
+    define_as = "BASE_HOLY_SYMBOL",
+    slot = "INVEN",
+    type = "symbol",
+    display = "(", color=colors.SLATE,
+    encumber = 10,
+    name = "holy symbol",
+    desc = "A holy symbol.",
+}
+
+newEntity{ base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Aiswin",
+    rarity = 15,
+}
+
+newEntity{ base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Asherath",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Ekliazeh",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Erich",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Essiah",
+    rarity = 15,
+}
+    
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Hesani",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Immotian",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Khasrach",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Kysul",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Maeve",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Mara",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Sabin",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Semirath",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of the Multitude",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Xavias",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Xel",
+    rarity = 15,
+}
+
+base = "BASE_HOLY_SYMBOL",
+    name = "holy symbol of Zurvash",
+    rarity = 15,
+}
+
+--Spellbooks!
+newEntity{
+    define_as = "BASE_SPELLBOOK",
+    slot = "INVEN",
+    type = "spellbook",
+    display = "(", color=colors.SLATE,
+    encumber = 10,
+    rarity = 15,
+    name = "spellbook",
+    desc = "A wizard's spellbook.",
+}
