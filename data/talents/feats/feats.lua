da4ce90fc479cc74dc12f26c25dceeace2655b2b
Zireael07 <zi@mail.com>
2015-02-03 20:50:06 +0100

    uncomment archery feats

--- data/talents/feats/feats.lua
+++ data/talents/feats/feats.lua
@@ -12,41 +12,55 @@ newTalentType{ type="class/general", no_tt_req = true, name = "General", descrip
 	is_feat = true,
 	fighter = true,]]
 --	info = [[This feat makes you better at shooting at close range, adding a +1 bonus.]],
---[[}
+--}
+
+newFeat{
+    name = "Precise Shot",
+    type = {"class/general", 1},
+    points = 1,
+    mode = "passive",
+    is_feat = true,
+    fighter = true,
+    info = [[This feat allows you to shoot or throw at opponents in melee range without the penalty.]]
+}
+
 
 newFeat{
 	name = "Far Shot",
 	type = {"class/general", 1},
 	require = {
-		talent = { Talents.T_POINT_BLANK_SHOT },
+	--	talent = { Talents.T_POINT_BLANK_SHOT },
+        talent = { Talents.T_PRECISE_SHOT},
 	},
 	points = 1,
 	mode = "passive",
 	is_feat = true,
-	fighter = true,]]
---	info = [[This feat increases the range of your bow or crossbow by 1,5.]],
---[[}
+	fighter = true,
+	info = [[This feat increases the range of your bow or crossbow by 1,5.]],
+}
 
 newFeat{
 	name = "Rapid Shot",
 	type = {"class/general", 1},
 	require = {
 		stat = {dex = 13},
-		talent = { Talents.T_POINT_BLANK_SHOT },
+	--	talent = { Talents.T_POINT_BLANK_SHOT },
+        talent = { Talents.T_PRECISE_SHOT },
 	},
 	points = 1,
 	mode = "passive",
 	is_feat = true,
-	fighter = true,]]
---	info = [[This feat lets you make a second shot, but both shots have a -2 penalty.]],
---[[}
+	fighter = true,
+	info = [[This feat lets you make a second shot, but both shots have a -2 penalty.]],
+}
 
 newFeat{
 	name = "Manyshot",
 	type = {"class/general", 1},
 	require = {
 		stat = {dex = 17},
-		talent = { Talents.T_POINT_BLANK_SHOT, Talents.T_RAPID_SHOT },
+	--	talent = { Talents.T_POINT_BLANK_SHOT, Talents.T_RAPID_SHOT },
+        talent = { Talents.T_PRECISE_SHOT, Talents.T_RAPID_SHOT },
 		special = {
 			fct = function(self, t, offset) 
 			--Base attack bonus 6
@@ -59,9 +73,9 @@ newFeat{
 	points = 1,
 	mode = "passive",
 	is_feat = true,
-	fighter = true,]]
---	info = [[This feat lets you make a second shot, but both shots have a -2 penalty.]],
---}
+	fighter = true,
+	info = [[You fire two arrows at once at the same target, with a -4 penalty. For every five points of BAB above +6, you may add one more arrow, with a cumulative -2 penalty.]],
+}
 
 --Various feats
 newFeat{
