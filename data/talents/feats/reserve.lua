d81e0382c0a7ff9612381e9a9b2d01642c61ef71
Zireael07 <zi@mail.com>
2015-02-11 21:01:47 +0100

    change the sample reserve feat to something more original

--- data/talents/feats/reserve.lua
+++ data/talents/feats/reserve.lua
@@ -1,32 +1,32 @@
 newTalentType{ type="magic/reserve", no_tt_req = true, name = "reserve", description = "Reserve feats" }
 
 newFeat{
-	name = "Fiery Burst",
+	name = "Force Bolt",
 	type = {"magic/reserve", 1},
 	require = {
 	},
 	tactical = { ATTACK = 1 },
 	range = 5,
 	on_pre_use = function(self, t, silent)
-		return self:highestSpellDescriptor("fire") and true or false
+		return self:highestSpellDescriptor("force") and true or false
 	end,
 	target = function(self, t)
 		return {type="hit", range=self:getTalentRange(t), talent=t, display={display='*',color=colors.RED}}
 	end,
 	action = function(self, t)
-		local bt = self:highestSpellDescriptor("fire")
+		local bt = self:highestSpellDescriptor("force")
 		local tg = self:getTalentTarget(t)
 		local x, y = self:getTarget(tg)
 		if not x then return nil end
 
-		local damage = rng.dice(bt.level, 6)
-		self:project(tg, x, y, DamageType.FIRE, {dam=damage})
+		local damage = rng.dice(bt.level, 4)
+		self:project(tg, x, y, DamageType.FORCE, {dam=damage})
 
 		return true
 	end,
 	info = function(self, t)
-		local t = self:highestSpellDescriptor("fire")
-		return ([[As long as you have a fire spell of available to cast, you can create a burst of fire at range 5 that
-		deals 1d6 damage per levels of the highest fire spell available (currently %dd6)]]):format(t and t.level or 0)
+		local t = self:highestSpellDescriptor("force")
+		return ([[As long as you have a force spell of available to cast, you can create a bolt of force at range 5 that
+		deals 1d4 damage per level of the highest force spell available (currently %dd6)]]):format(t and t.level or 0)
 	end,
 }
