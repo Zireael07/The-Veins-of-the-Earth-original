151ded94cf975e902a6052fb98f259161118957e
Zireael07 <zi@mail.com>
2015-03-07 15:22:14 +0100

    remove unused duplicate code

--- data/general/ego/egos.lua
+++
@@ -1,8 +0,0 @@
---Veins of the Earth
--- Zireael 2014
-
-load("data/general/objects/properties/armor.lua")
-load("data/general/objects/properties/lite.lua")
-load("data/general/objects/properties/shields.lua")
-load("data/general/objects/properties/weapons.lua")
-load("data/general/objects/properties/wondrous_items.lua")
