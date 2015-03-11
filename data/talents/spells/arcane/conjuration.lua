43260404ff6be0de4145c8d4f362a63b6bcb4dc0
Zireael07 <zi@mail.com>
2015-03-11 17:30:40 +0100

    fix Summon Monster at long last!

--- data/talents/spells/arcane/conjuration.lua
+++ data/talents/spells/arcane/conjuration.lua
@@ -1,4 +1,4 @@
-newTalentType{ 
+newTalentType{
 	all_limited=true,
 	type="conjuration",
 	name="conjuration",
@@ -14,7 +14,7 @@ newArcaneSpell{
 	cooldown = 8,
 	tactical = { BUFF = 2 },
 	getDamage = function(self, t)
-		if self:isTalentActive(self.T_MAXIMIZE) then return 3 
+		if self:isTalentActive(self.T_MAXIMIZE) then return 3
 		elseif self:isTalentActive(self.T_EMPOWER) then return math.max(rng.dice(1,3)*1.5)
 		else return rng.dice(1,3) end
 	end,
@@ -41,7 +41,7 @@ newArcaneSpell{
 	end,
 }
 
-newArcaneSpell{	
+newArcaneSpell{
 	name = "Grease",
 	type = {"conjuration", 1},
 	mode = 'activated',
@@ -49,8 +49,8 @@ newArcaneSpell{
 	points = 1,
 	cooldown = 0,
 	tactical = { BUFF = 2 },
-	getDuration = function(self, t)  
-		if self:isTalentActive(self.T_EXTEND) then return 8 
+	getDuration = function(self, t)
+		if self:isTalentActive(self.T_EXTEND) then return 8
 		else return 5 end
 	end,
 	range = 5,
@@ -112,17 +112,17 @@ newArcaneSpell{
 			desc = [[A peaceful horse.]],
 			--autolevel = "none",
 			never_anger = true,
-			
+
 			ai = "ally",
 			ai_state = { talent_in=1, ally_compassion=10},
 			ai_tactic = resolvers.tactic"default",
-			
+
 			stats = {str=0, dex=0, con=0, int=0, wis=0, cha=0, luc=0},
 			inc_stats = {
 				str = 14,
 				dex = 13,
 				con = 15,
-				int = 2, 
+				int = 2,
 				wis = 12,
 				cha = 6,
 				luc = 10,
@@ -142,7 +142,7 @@ newArcaneSpell{
 		}
 		return m
 		end
-		
+
 		if creature == "pony" then
 			local m = NPC.new{
 			type = "animal", subtype = "horse",
@@ -152,17 +152,17 @@ newArcaneSpell{
 			desc = [[A large peaceful pony.]],
 			--autolevel = "none",
 			never_anger = true,
-			
+
 			ai = "ally",
 			ai_state = { talent_in=1, ally_compassion=10},
 			ai_tactic = resolvers.tactic"default",
-			
+
 			stats = {str=0, dex=0, con=0, int=0, wis=0, cha=0, luc=0},
 			inc_stats = {
 				str = 13,
 				dex = 13,
 				con = 12,
-				int = 2, 
+				int = 2,
 				wis = 11,
 				cha = 4,
 				luc = 10,
@@ -192,7 +192,7 @@ newArcaneSpell{
 			self:talentDialogReturn(result)
 			game:unregisterDialog(self:talentDialogGet())
 		end))
-		
+
 		if not result then return nil end
 
 		local tg = self:getTalentTarget(t)
@@ -206,8 +206,8 @@ newArcaneSpell{
 			return nil
 		end
 		if not x or not y then
-			game.logPlayer(self, "You cannot summon there.") 
-			return nil 
+			game.logPlayer(self, "You cannot summon there.")
+			return nil
 		end
 
 		game.logPlayer(self, ("Player summons a %s!"):format(result))
@@ -230,14 +230,259 @@ newArcaneSpell{
 	points = 1,
 	cooldown = 0,
 	range = 3,
-	setCreature = function(t, creature)
-		t.creature = creature
-	end,
 	target = function(self, t)
 		return {type="hit", range=self:getTalentRange(t), nolock = true, talent=t}
 	end,
-	makeCreature = function(self, t)
+	makeCreature = function(self, t, creature)
 		local NPC = require "mod.class.NPC"
+		if creature == "pony" then
+			local m = NPC.new{
+			type = "animal", subtype = "horse",
+			display = "q", color=colors.LIGHT_GREEN,
+			image = "tiles/horse_spotted.png",
+			name = "pony mount", faction = self.faction,
+			desc = [[A large peaceful pony.]],
+			--autolevel = "none",
+			never_anger = true,
+
+			ai = "ally",
+			ai_state = { talent_in=1, ally_compassion=10},
+			ai_tactic = resolvers.tactic"default",
+
+			stats = {str=0, dex=0, con=0, int=0, wis=0, cha=0, luc=0},
+			inc_stats = {
+				str = 13,
+				dex = 13,
+				con = 12,
+				int = 2,
+				wis = 11,
+				cha = 4,
+				luc = 10,
+					},
+			level_range = {self.level, self.level}, exp_worth = 0,
+
+			max_life = 11,
+
+			combat_base_ac = 10, combat_dr = 0, combat_natural = 2,
+			combat = { dam={1,3}, atk=1, },
+			movement_speed = 1.33,
+			mount = true,
+
+			summoner = self, summoner_gain_exp=true,
+			summon_time = 10,
+			ai_target = {actor=target},
+		}
+		return m
+		end
+		if creature == "dire rat" then
+			local m = NPC.new{
+			type = "animal", subtype = "rat",
+			display = "r", color=colors.LIGHT_GREEN,
+			image = "tiles/rat.png",
+			name = "dire rat", faction = self.faction,
+			desc = [[A large peaceful rat.]],
+			--autolevel = "none",
+			never_anger = true,
+
+			ai = "ally",
+			ai_state = { talent_in=1, ally_compassion=10},
+			ai_tactic = resolvers.tactic"default",
+
+			stats = {str=0, dex=0, con=0, int=0, wis=0, cha=0, luc=0},
+			inc_stats = {
+				str = 10,
+				dex = 17,
+				con = 12,
+				int = 1,
+				wis = 12,
+				cha = 4,
+				luc = 2,
+					},
+			level_range = {self.level, self.level}, exp_worth = 0,
+
+			max_life = 7,
+
+			combat_base_ac = 10, combat_dr = 0, combat_natural = 2,
+			combat = { dam={1,3}, atk=1, },
+
+			summoner = self, summoner_gain_exp=true,
+			summon_time = 10,
+			ai_target = {actor=target},
+		}
+		return m
+		end
+		if creature == "medium spider" then
+			local m = NPC.new{
+			type = "vermin", subtype = "spider",
+			display = "r", color=colors.LIGHT_GREEN,
+			image = "tiles/spider.png",
+			name = "small spider", faction = self.faction,
+			desc = [[A large peaceful spider.]],
+			--autolevel = "none",
+			never_anger = true,
+
+			ai = "ally",
+			ai_state = { talent_in=1, ally_compassion=10},
+			ai_tactic = resolvers.tactic"default",
+
+			stats = {str=0, dex=0, con=0, int=0, wis=0, cha=0, luc=0},
+			inc_stats = {
+				str = 11,
+				dex = 17,
+				con = 10,
+				int = 1,
+				wis = 10,
+				cha = 2,
+				luc = 10,
+					},
+			level_range = {self.level, self.level}, exp_worth = 0,
+
+			max_life = 7,
+
+			combat_base_ac = 10, combat_dr = 0, combat_natural = 2,
+			combat = { dam={1,6}, atk=1, },
+			poison = "medium_spider",
+
+			summoner = self, summoner_gain_exp=true,
+			summon_time = 10,
+			ai_target = {actor=target},
+		}
+		return m
+		end
+	end,
+	action = function(self, t)
+
+		local result = self:talentDialog(require('mod.dialogs.GetChoice').new("Choose the desired summon",{
+			{name="pony", desc=""},
+			{name="dire rat", desc=""},
+			{name="medium spider", desc=""},
+		}, function(result)
+			self:talentDialogReturn(result)
+			game:unregisterDialog(self:talentDialogGet())
+		end))
+
+		if not result then return nil end
+
+		local tg = self:getTalentTarget(t)
+		local x, y =  self:getTarget(tg)
+		if not x or not y then return nil end
+
+		local _ _, _, _, x, y = self:canProject(tg, x, y)
+		local blocked = game.level.map(x, y, Map.ACTOR)
+		if blocked then
+			game.logPlayer(self, "You must summon on an empty square!")
+			return nil
+		end
+		if not x or not y then
+			game.logPlayer(self, "You cannot summon there")
+			return nil
+		end
+
+		game.logPlayer(self, ("Player summons a %s!"):format(result))
+
+--[[		if t.creature then
+			game.logPlayer(self,("Player summons a %s!"):format(t.creature))
+		else
+			game.logPlayer(self,"Player doesn't summon a creature")
+		end]]
+
+		local creature = t.makeCreature(self, t, result)
+		game.zone:addEntity(game.level, creature, "actor", x, y)
+		return true
+	end,
+	info = function(self, t)
+		return ([[You summon a single creature to do your bidding.]])
+	end,
+}
+
+newArcaneSpell{
+	name = "Mage Armor",
+	type = {"conjuration", 1},
+	mode = 'activated',
+	level = 1,
+	points = 1,
+	cooldown = 0,
+	tactical = { BUFF = 2 },
+	getDuration = function(self, t)
+		if self:isTalentActive(self.T_EXTEND) then return 15
+		else return 10 end
+	end,
+	range = 1,
+	requires_target = true,
+	target = function(self, t)
+		return {type="hit", range=self:getTalentRange(t), selffire=false, talent=t}
+	end,
+	action = function(self, t)
+		local tg = {type="hit", range=self:getTalentRange(t)}
+		local x, y, target = self:getTarget(tg)
+		if not x or not y or not target then return nil end
+	--	if core.fov.distance(self.x, self.y, x, y) > 1 then return nil end
+
+		target:setEffect(self.EFF_MAGE_ARMOR, t.getDuration(self, t), {})
+
+		return true
+	end,
+	info = function(self, t)
+
+		return ([[An invisible but tangible field of force surrounds the target, providing a +4 armor bonus to AC.]])
+	end,
+}
+
+newArcaneSpell{
+	name = "Summon Creature II",
+	type = {"conjuration", 1},
+	mode = "activated",
+	level = 2,
+	points = 1,
+	cooldown = 0,
+	range = 3,
+	target = function(self, t)
+		return {type="hit", range=self:getTalentRange(t), nolock = true, talent=t}
+	end,
+	makeCreature = function(self, t, creature)
+		local NPC = require "mod.class.NPC"
+		if creature == "light horse" then
+
+		local m = NPC.new{
+			type = "animal", subtype = "horse",
+			display = "q", color=colors.LIGHT_GREEN,
+			image = "tiles/horse_brown.png",
+			name = "light horse mount", faction = self.faction,
+			desc = [[A peaceful horse.]],
+			--autolevel = "none",
+			never_anger = true,
+
+			ai = "ally",
+			ai_state = { talent_in=1, ally_compassion=10},
+			ai_tactic = resolvers.tactic"default",
+
+			stats = {str=0, dex=0, con=0, int=0, wis=0, cha=0, luc=0},
+			inc_stats = {
+				str = 14,
+				dex = 13,
+				con = 15,
+				int = 2,
+				wis = 12,
+				cha = 6,
+				luc = 10,
+					},
+			level_range = {self.level, self.level}, exp_worth = 0,
+
+			max_life = 19,
+
+			combat_base_ac = 10, combat_dr = 0, combat_natural = 2,
+			combat = { dam={1,4}, atk=1, },
+			movement_speed = 1.66,
+			mount = true,
+
+			summoner = self, summoner_gain_exp=true,
+			summon_time = 10,
+			ai_target = {actor=target},
+		}
+		return m
+		end
+
+		if creature == "wolf" then
 		local m = NPC.new{
 			type = "animal", subtype = "wolf",
 			display = "q", color=colors.LIGHT_GREEN,
@@ -245,17 +490,17 @@ newArcaneSpell{
 			desc = [[A large peaceful wolf.]],
 			--autolevel = "none",
 			never_anger = true,
-			
+
 			ai = "ally",
 			ai_state = { talent_in=1, ally_compassion=10},
 			ai_tactic = resolvers.tactic"default",
-			
+
 			stats = {str=0, dex=0, con=0, int=0, wis=0, cha=0, luc=0},
 			inc_stats = {
 				str = 13,
 				dex = 15,
 				con = 15,
-				int = 2, 
+				int = 2,
 				wis = 12,
 				cha = 6,
 				luc = 10,
@@ -272,20 +517,19 @@ newArcaneSpell{
 			ai_target = {actor=target},
 		}
 		return m
+		end
 	end,
 	action = function(self, t)
-		-- Choose creature
-		local d = require("mod.dialogs.SummonCreatureI").new(t)
 
-		local co = coroutine.running()
-		d.unload = function() coroutine.resume(co, t.creature) end --This is currently bugged, only works if the player has already summoned,
+		local result = self:talentDialog(require('mod.dialogs.GetChoice').new("Choose the desired summon",{
+			{name="wolf", desc=""},
+			{name="light horse", desc=""},
+		}, function(result)
+			self:talentDialogReturn(result)
+			game:unregisterDialog(self:talentDialogGet())
+		end))
 
-		--No more dialog spam for NPCs
-		if game.player then 
-		game:registerDialog(d)
-		end
-		
-		if not coroutine.yield() then return nil end
+		if not result then return nil end
 
 		local tg = self:getTalentTarget(t)
 		local x, y =  self:getTarget(tg)
@@ -298,61 +542,29 @@ newArcaneSpell{
 			return nil
 		end
 		if not x or not y then
-			game.logPlayer(self, "You cannot summon there") 
-			return nil 
+			game.logPlayer(self, "You cannot summon there")
+			return nil
 		end
 
-		if t.creature then
+		game.logPlayer(self, ("Player summons a %s!"):format(result))
+
+--[[		if t.creature then
 			game.logPlayer(self,("Player summons a %s!"):format(t.creature))
 		else
 			game.logPlayer(self,"Player doesn't summon a creature")
-		end
+		end]]
 
-		local creature = t.makeCreature(self, t)
+		local creature = t.makeCreature(self, t, result)
 		game.zone:addEntity(game.level, creature, "actor", x, y)
 		return true
-
-		
 	end,
 	info = function(self, t)
 		return ([[You summon a single creature to do your bidding.]])
 	end,
 }
 
-newArcaneSpell{	
-	name = "Mage Armor",
-	type = {"conjuration", 1},
-	mode = 'activated',
-	level = 2,
-	points = 1,
-	cooldown = 0,
-	tactical = { BUFF = 2 },
-	getDuration = function(self, t)  
-		if self:isTalentActive(self.T_EXTEND) then return 15 
-		else return 10 end
-	end,
-	range = 1,
-	requires_target = true,
-	target = function(self, t)
-		return {type="hit", range=self:getTalentRange(t), selffire=false, talent=t}
-	end,
-	action = function(self, t)
-		local tg = {type="hit", range=self:getTalentRange(t)}
-		local x, y, target = self:getTarget(tg)
-		if not x or not y or not target then return nil end
-	--	if core.fov.distance(self.x, self.y, x, y) > 1 then return nil end
 
-		target:setEffect(self.EFF_MAGE_ARMOR, t.getDuration(self, t), {})
-		
-		return true
-	end,
-	info = function(self, t)
-
-		return ([[An invisible but tangible field of force surrounds the target, providing a +4 armor bonus to AC.]])
-	end,
-}
-
-newArcaneSpell{	
+newArcaneSpell{
 	name = "Dimension Door",
 	type = {"conjuration", 1},
 	mode = 'activated',
@@ -381,7 +593,7 @@ newArcaneSpell{
 
 
 	--	local x, y = self.x, self.y
-		
+
 		return true
 	end,
 	info = function(self, t)
