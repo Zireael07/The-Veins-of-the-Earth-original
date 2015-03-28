ea2599e40e6338c1dd9d71c8a0e9292b62a2ac62
Zireael07 <zi@mail.com>
2015-03-28 12:51:07 +0100

    damage logged now includes all increases

--- data/damage_types.lua
+++ data/damage_types.lua
@@ -22,48 +22,45 @@ setDefaultProjector(function(src, x, y, type, dam)
 	local target = game.level.map(x, y, Map.ACTOR)
 	if target then
 
-
 		if dam > 0 then
+			local sx, sy = game.level.map:getTileToScreen(x, y)
+			if target:takeHit(dam, src) then
+				if src == game.player or target == game.player then
+					game.flyers:add(sx, sy, 30, (rng.range(0,2)-1) * 0.5, -3, "Kill!", {255,0,255})
+				end
+			else
+				if src == game.player then
+					game.flyers:add(sx, sy, 30, (rng.range(0,2)-1) * 0.5, -3, tostring(-math.ceil(dam)), {0,255,0})
+				elseif target == game.player then
+					game.flyers:add(sx, sy, 30, (rng.range(0,2)-1) * 0.5, -3, tostring(-math.ceil(dam)), {255,0,0})
+				end
+			end
 
-		-- Log damage for later
-		if not DamageType:get(type).hideMessage then
-			local visible, srcSeen, tgtSeen = game:logVisible(src, target)
-			if visible then -- don't log damage that the player doesn't know about
+			--Damage increases
+			if src.inc_damage then
+				local inc
+				inc = (src.inc_damage.all or 0) + (src.inc_damage[type] or 0)
+
+				dam = dam + (dam * inc / 100)
+			end
+
+			-- Log damage for later
+			if not DamageType:get(type).hideMessage then
+				local visible, srcSeen, tgtSeen = game:logVisible(src, target)
+				if visible then -- don't log damage that the player doesn't know about
 				local source = src.__project_source or src
 
-				game.logSeen(target, "%s hits %s for %s%0.2f %s damage#LAST#.", src.name:capitalize(), target.name, DamageType:get(type).text_color or "#aaaaaa#", dam, DamageType:get(type).name)
+					game.logSeen(target, "%s hits %s for %s%0.2f %s damage#LAST#.", src:getLogName():capitalize(), target:getLogName(), DamageType:get(type).text_color or "#aaaaaa#", dam, DamageType:get(type).name)
 
 			--[[	if src.turn_procs and src.turn_procs.is_crit then
 					game:delayedLogDamage(source, target, dam, ("#{bold}#%s%d %s#{normal}##LAST#"):format(DamageType:get(type).text_color or "#aaaaaa#", dam, DamageType:get(type).name), true)
 				else
 					game:delayedLogDamage(source, target, dam, ("%s%d %s#LAST#"):format(DamageType:get(type).text_color or "#aaaaaa#", dam, DamageType:get(type).name), false)
 			--	end]]
+				end
 			end
-		end
 
 		end
-		
-		local sx, sy = game.level.map:getTileToScreen(x, y)
-		if target:takeHit(dam, src) then
-			if src == game.player or target == game.player then
-				game.flyers:add(sx, sy, 30, (rng.range(0,2)-1) * 0.5, -3, "Kill!", {255,0,255})
-			end
-		else
-			if src == game.player then
-				game.flyers:add(sx, sy, 30, (rng.range(0,2)-1) * 0.5, -3, tostring(-math.ceil(dam)), {0,255,0})
-			elseif target == game.player then
-				game.flyers:add(sx, sy, 30, (rng.range(0,2)-1) * 0.5, -3, tostring(-math.ceil(dam)), {255,0,0})
-			end
-		end
-
-		--Damage increases
-		if src.inc_damage then
-			local inc
-			inc = (src.inc_damage.all or 0) + (src.inc_damage[type] or 0)
-
-			dam = dam + (dam * inc / 100)
-		end
-
 
 		return dam
 	end
@@ -228,7 +225,7 @@ newDamageType{
 	projector = function(src, x, y, type, dam)
 		local realdam = DamageType.defaultProjector(src, x, y, type, dam)
 		local target = game.level.map(x, y, Map.ACTOR)
-		if target then 
+		if target then
 			target:setEffect(target.EFF_DARKNESS, 1, {})
 			-- Darken
 			game.level.map.lites(x, y, false)
@@ -272,7 +269,7 @@ newDamageType{
 		game.log("Target glows red briefly.")
 		target:setEffect(target.EFF_DETECT_EVIL, 3, {})
 		else
-		game.log("Target is not evil.")	
+		game.log("Target is not evil.")
 		end
 	end
 	end,
@@ -287,7 +284,7 @@ newDamageType{
 		game.log("Target glows blue briefly.")
 		target:setEffect(target.EFF_DETECT_GOOD, 3, {})
 		else
-		game.log("Target is not good.")	
+		game.log("Target is not good.")
 		end
 	end
 	end,
@@ -302,7 +299,7 @@ newDamageType{
 		game.log("Target glows green briefly.")
 		target:setEffect(target.EFF_DETECT_CHAOS, 3, {})
 		else
-		game.log("Target is not chaotic.")	
+		game.log("Target is not chaotic.")
 		end
 	end
 	end,
@@ -317,7 +314,7 @@ newDamageType{
 		game.log("Target glows yellow briefly.")
 		target:setEffect(target.EFF_DETECT_LAW, 3, {})
 		else
-		game.log("Target is not lawful.")	
+		game.log("Target is not lawful.")
 		end
 	end
 	end,
