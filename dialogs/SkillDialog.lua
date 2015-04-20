db1b11d6b5d16a87172615acb7ad5f0e139cacf2
Zireael07 <zi@mail.com>
2015-04-20 08:48:33 +0200

    use the new ActorSkills in skill select screen, ripping out fat local tables

--- dialogs/SkillDialog.lua
+++ dialogs/SkillDialog.lua
@@ -10,6 +10,8 @@ local Player = require "mod.class.Player"
 local UI = require "engine.ui.Base"
 local List = require "engine.ui.List"
 
+local ActorSkills = require "mod.class.interface.ActorSkills"
+
 
 module(..., package.seeall, class.inherit(Dialog))
 
@@ -25,45 +27,13 @@ local function restore(dest, backup)
     if game.level and dest.x then game.level.map:updateMap(dest.x, dest.y) end
 end
 
-local skills = {
-	appraise = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Intelligence#LAST#\n\nUsed for haggling and determining the price for your items.',
-    balance = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Dexterity#LAST#\n\nUsed for walking on dangerous terrain.',
-	bluff = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Charisma#LAST#\n\nUsed when lying to NPCs.',
-	climb = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Strength#LAST#\n\nUsed for climbing over chasms.',
-	concentration = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Constitution#LAST#\n\nUsed when spellcasting under duress or while threatened.',
-	craft = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Intelligence#LAST#\n\nUsed when crafting or repairing mundane items.',
-    decipherscript = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Intelligence#LAST#\n\nUsed when deciphering runes or scrolls.',
-    diplomacy = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Charisma#LAST#\n\nUsed when negotiating or persuading NPCs.',
-	disabledevice = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Intelligence#LAST#\n\n#STEEL_BLUE#Rogues only!#LAST# Used to disable traps.',
-	escapeartist = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Dexterity#LAST#\n\nUsed to avoid being entangled or tied up.',
-	handleanimal = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Wisdom#LAST#\n\nUsed when dealing with animals.',
-	heal = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Wisdom#LAST#\n\nUsed to heal yourself using a healer kit.',
-	hide = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Dexterity#LAST#\n\nUsed to hide from enemies.',
-	intimidate = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Charisma#LAST#\n\nUsed to frighten enemies or to intimidate others.',
-	intuition = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Intelligence#LAST#\n\nUsed for identifying items',
-	jump = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Strength#LAST#\n\nUsed for clearing obstacles.',
-	knowledge = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Wisdom#LAST#\n\nUsed for various bits of lore.',
-	listen = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Wisdom#LAST#\n\nUsed to detect enemies from a distance.',
-	movesilently = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Dexterity#LAST#\n\nUsed to hide from enemies.',
-	openlock = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Dexterity#LAST#\n\nUsed to open locks.',
-	pickpocket = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Dexterity#LAST#\n\n#STEEL_BLUE#Rogues only!#LAST# Used to steal from NPCs.',
-	ride = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Dexterity#LAST#\n\nUsed to ride mounts.',
-	search = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Intelligence#LAST#\n\nUsed to find traps. #STEEL_BLUE#Only rogues can find traps with a DC over 20.#LAST#',
-	sensemotive = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Wisdom#LAST#\n\nUsed to detect if someone is lying.',
-	swim = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE#Strength#LAST#\n\nUsed to avoid drowning in water.',
-	spellcraft = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Intelligence#LAST#\n\nUsed to identify magical effects or spells being cast by others.',
-	survival = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Wisdom#LAST#\n\nUsed to track, forage and to survive in the wild.',
-	tumble = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Dexterity#LAST#\n\nUsed to avoid blows.',
-	usemagic = '#TAN#Uses stat:#LAST##SANTIQUE_WHITE# Intelligence#LAST#\n\n#STEEL_BLUE#Rogues only!#LAST# Used to manipulate magic items.',
-}
-
 function _M:init(actor)
 	self.player = actor
 	self.actor = actor
     self.actor_dup = actor:clone()
 	Dialog.init(self, "Skills", game.w*0.5, game.h*0.6)
 	self:generateList()
-	
+
 	self.c_points = Textzone.new{width=self.iw, auto_height=true, text = "Available skill points: #GOLD#"..self.player.skill_point.. " #LAST#Max skill ranks: #GOLD#"..self.player.max_skill_ranks.."\n#LAST#Only skill ranks are displayed.\n Skills in blue are cross-class skills."}
 	self.c_list = List.new{width=self.iw/2, height=self.ih-self.c_points.h - 10, nb_items=#self.list, list=self.list, fct=function(item) self:use(item) end, select=function(item,sel) self:on_select(item,sel) end, scrollbar=true}
 	self.c_desc = TextzoneList.new{width=self.iw/2-20, height = 400, text="Hello from description"}
@@ -111,11 +81,11 @@ end
 function _M:use(item)
 	--check that we don't get negative values AFTER deducting a point
 	if (self.player.skill_point or 0) -1 >= 0 then
-		
+
 	--Cross class skills
 	if self.player:crossClass(item.skill) then
 		if (self.player:attr("skill_"..item.skill) or 0) < self.player.cross_class_ranks then
-		
+
 		--increase the skill by one
 		self.player:attr("skill_"..item.skill, 1)
 
@@ -124,7 +94,7 @@ function _M:use(item)
 		end
 	else
 
-	--Class skills	
+	--Class skills
 	if (self.player:attr("skill_"..item.skill) or 0) < self.player.max_skill_ranks then
 
 		--increase the skill by one
@@ -139,7 +109,7 @@ end
 
 function _M:on_select(item,sel)
 	if self.c_desc then self.c_desc:switchItem(item, item.desc) end
-	self.selection = sel	
+	self.selection = sel
 end
 
 function _M:update()
@@ -155,29 +125,36 @@ end
 function _M:generateList()
 	local player = game.player
     local list = {}
-    for skill, description in pairs(skills) do
+
+    for i, s in ipairs(ActorSkills.skill_defs) do
+        local skill = s.id
     	local value = self.player:attr("skill_"..skill)
  		local color = {255, 255, 255}
 
- 		local d = "#CHOCOLATE#"..skill:capitalize().."#LAST#\n\n"
- 		d = d..description.."\n#WHITE#"
-        if self.player:crossClass(skill) then 
+        local d = "#CHOCOLATE#"..s.name:capitalize().."#LAST#\n\n"
+
+        d = d.."#TAN#Uses stat:#LAST##SANTIQUE_WHITE# "..s.stat:capitalize().."#LAST#\n\n"..s.desc.."\n#WHITE#"
+        if s.rogue_only then
+            d = d.."#STEEL_BLUE#Rogues only!#LAST#"
+        end
+
+        if self.player:crossClass(skill) then
             --show value in gold if it's maxed out
             if (self.player:attr("skill_"..skill) or 0) == self.player.cross_class_ranks then
-            name = "#SLATE#(#LAST##GOLD#"..(value or 0).."#LAST##SLATE#) #LAST##LIGHT_BLUE#"..skill:capitalize()
+                name = "#SLATE#(#LAST##GOLD#"..(value or 0).."#LAST##SLATE#) #LAST##LIGHT_BLUE#"..s.name:capitalize()
             else
-            name = "#SLATE#(#LAST##ORANGE#"..(value or 0).."#LAST##SLATE#) #LAST##LIGHT_BLUE#"..skill:capitalize()
-            end	
-        else 
+                name = "#SLATE#(#LAST##ORANGE#"..(value or 0).."#LAST##SLATE#) #LAST##LIGHT_BLUE#"..s.name:capitalize()
+            end
+        else
             --show value in gold if it's maxed out
             if (self.player:attr("skill_"..skill) or 0) == self.player.max_skill_ranks then
-            name = "#SLATE#(#LAST##GOLD#"..(value or 0).."#LAST##SLATE#) #LAST#"..skill:capitalize()
+                name = "#SLATE#(#LAST##GOLD#"..(value or 0).."#LAST##SLATE#) #LAST#"..s.name:capitalize()
             else
-            name = "#SLATE#(#LAST##ORANGE#"..(value or 0).."#LAST##SLATE#) #LAST#"..skill:capitalize()	
+                name = "#SLATE#(#LAST##ORANGE#"..(value or 0).."#LAST##SLATE#) #LAST#"..s.name:capitalize()
             end
         end
-        	list[#list+1] = {name=name, skill = skill,	 color = color, desc=d}
+        	list[#list+1] = {name=name, skill = skill, color = color, desc=d}
     end
     self.list = list
     table.sort(list, function(a,b) return a.skill < b.skill end)
-end	
+end
