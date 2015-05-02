919853fe5ace52324cc57dd95a9c8e07c3bda650
Zireael07 <zi@mail.com>
2015-05-02 17:44:26 +0200

    add chromatic and metallic dragons

--- data/general/npcs/draconic.lua
+++ data/general/npcs/draconic.lua
@@ -112,3 +112,219 @@ newEntity{
         fly = true,
         resolvers.wounds()
 }
+
+--NOTE: True dragons start here
+--Immunity to acid, water breathing
+newEntity{
+	define_as = "BASE_NPC_DRAGON_BLACK",
+	type = "dragon",
+	display = 'D', color=colors.BLACK,
+	body = { INVEN = 10 },
+	desc = [[Black dragons are sometimes known as skull dragons because of their skeletal faces. Adding to the skeletal impression is the gradual deterioration of the hide around the base of the horn and the cheekbones. This deterioration increases with age and does not harm the dragon. On hatching, a black dragon�s scales are thin, small, and glossy. As the dragon ages, they become larger, thicker, and duller, helping it camouflage itself in swamps and marshes.]],
+	ai = "human_level", ai_state = { talent_in=3, },
+    stats = { str=11, dex=10, con=13, int=8, wis=11, cha=8, luc=10 },
+    combat = { dam= {1,6} },
+    name = "black dragon",
+    level_range = {3, nil}, exp_worth = 900,
+    rarity = 15,
+    max_life = resolvers.rngavg(27,32),
+    hit_die = 4,
+    challenge = 3,
+    infravision = 4,
+    combat_natural = 3,
+    --TODO: skills
+    movement_speed = 2,
+    alignment = "Chaotic Evil",
+    resolvers.wounds()
+}
+
+--Immunity to electricity, create/destroy water
+newEntity{
+	define_as = "BASE_NPC_DRAGON_BLUE",
+	type = "dragon",
+	display = 'D', color=colors.BLUE,
+	body = { INVEN = 10 },
+	desc = [[A blue dragon's scales vary in color from an iridescent azure to a deep indigo, polished to a glossy finish by blowing desert sands. The size of its scales increases little as the dragon ages, although they do become thicker and harder. Its hide tends to hum and crackle faintly with built-up static electricity. These effects intensify when the dragon is angry or about to attack, giving off an odor of ozone and sand. Their vibrant color makes blue dragons easy to spot in barren desert surroundings. However, they often burrow into the sand so only part of their heads are exposed.]],
+	ai = "human_level", ai_state = { talent_in=3, },
+    stats = { str=13, dex=10, con=13, int=10, wis=11, cha=10, luc=10 },
+    combat = { dam= {1,6} },
+    name = "blue dragon",
+    level_range = {3, nil}, exp_worth = 900,
+    rarity = 15,
+    max_life = resolvers.rngavg(43,48),
+    hit_die = 4,
+    challenge = 3,
+    infravision = 4,
+    combat_natural = 5,
+ 	--TODO: skills
+	movement_speed = 1.33,
+	alignment = "Lawful Evil",
+	resolvers.wounds()
+}
+
+newEntity{
+	define_as = "BASE_NPC_DRAGON_GREEN",
+	type = "dragon",
+	display = 'D', color=colors.GREEN,
+	body = { INVEN = 10 },
+	desc = [[A wyrmling green dragon's scales are thin, very small, and a deep shade of green that appears nearly black. As the dragon ages, the scales grow larger and lighter, turning shades of forest, emerald, and olive green, which helps it blend in with its wooded surroundings.]],
+	ai = "human_level", ai_state = { talent_in=3, },
+    stats = { str=13, dex=10, con=13, int=10, wis=11, cha=10, luc=10 },
+    combat = { dam= {1,6} },
+    name = "blue dragon",
+    level_range = {3, nil}, exp_worth = 900,
+    rarity = 15,
+    max_life = resolvers.rngavg(35,40),
+    hit_die = 5,
+    challenge = 3,
+    infravision = 4,
+    combat_natural = 5,
+ 	--TODO: skills
+	movement_speed = 1.33,
+	alignment = "Lawful Evil",
+	resolvers.wounds()
+}
+
+newEntity{
+	define_as = "BASE_NPC_DRAGON_RED",
+	type = "dragon",
+	display = 'D', color=colors.RED,
+	body = { INVEN = 10 },
+	desc = [[The small scales of a wyrmling red dragon are a bright glossy scarlet, making the dragon easily spotted by predators and hunters, so it stays underground and does not venture outside until it is more able to take care of itself. Toward the end of young age, the scales turn a deeper red, and the glossy texture is replaced by a smooth, dull finish. As the dragon grows older, the scales become large, thick, and as strong as metal. The neck frill and wings are an ash blue or purple-gray toward the edges, becoming darker with age. The pupils of a red dragon fade as it ages; the oldest red dragons have eyes that resemble molten lava orbs.]],
+	ai = "human_level", ai_state = { talent_in=3, },
+    stats = { str=17, dex=10, con=15, int=10, wis=11, cha=10, luc=10 },
+    combat = { dam= {1,6} },
+    name = "red dragon",
+    level_range = {3, nil}, exp_worth = 900,
+    rarity = 15,
+    max_life = resolvers.rngavg(55,60),
+    hit_die = 7,
+    challenge = 3,
+    infravision = 4,
+    combat_natural = 6,
+ 	--TODO: skills
+	movement_speed = 1.33,
+	alignment = "Chaotic Evil",
+	resolvers.wounds()
+}
+
+--Icewalking, immunity to cold, vulnerability to fire
+newEntity{
+	define_as = "BASE_NPC_DRAGON_WHITE",
+	type = "dragon",
+	display = 'D', color=colors.WHITE,
+	body = { INVEN = 10 },
+	desc = [[The scales of a wyrmling white dragon glisten like mirrors. As the dragon ages, the sheen disappears, and by very old age, scales of pale blue and light gray are mixed in with the white.]],
+	ai = "human_level", ai_state = { talent_in=3, },
+    stats = { str=17, dex=10, con=15, int=10, wis=11, cha=10, luc=10 },
+    combat = { dam= {1,6} },
+    name = "white dragon",
+    level_range = {3, nil}, exp_worth = 900,
+    rarity = 15,
+    max_life = resolvers.rngavg(20,25),
+    hit_die = 3,
+    challenge = 3,
+    infravision = 4,
+    combat_natural = 2,
+ 	--TODO: skills
+	movement_speed = 1.33,
+	alignment = "Chaotic Evil",
+	resolvers.wounds()
+}
+
+--Metallic
+--Immunity to fire, vulnerability to cold, speak with animals
+newEntity{
+	define_as = "BASE_NPC_DRAGON_BRASS",
+	type = "dragon",
+	display = 'D', color=colors.SANDY_BROWN,
+	body = { INVEN = 10 },
+	desc = [[At birth, a brass dragon's scales are a dull, mottled brown. As the dragon gets older, the scales become more brassy until they reach a warm, burnished appearance. The grand head-plates of a brass dragon are smooth and metallic, and it sports bladed chin horns that grow sharper with age. Wings and frills are mottled green toward the edges, darkening with age. As the dragon grows older, its pupils fade until the eyes resemble molten metal orbs.]],
+	ai = "human_level", ai_state = { talent_in=3, },
+    stats = { str=11, dex=10, con=13, int=10, wis=11, cha=10, luc=10 },
+    combat = { dam= {1,6} },
+    name = "brass dragon",
+    level_range = {3, nil}, exp_worth = 900,
+    rarity = 25,
+    max_life = resolvers.rngavg(28,33),
+    hit_die = 4,
+    challenge = 3,
+    infravision = 4,
+    combat_natural = 3,
+ 	--TODO: skills
+	movement_speed = 2,
+	alignment = "Chaotic Good",
+	resolvers.wounds()
+}
+
+--Immunity to acid, spider climb
+newEntity{
+	define_as = "BASE_NPC_DRAGON_COPPER",
+	type = "dragon",
+	display = 'D', color=colors.DARK_TAN,
+	body = { INVEN = 10 },
+	desc = [[A bronze wyrmling�s scales are yellow tinged with green, showing only a hint of bronze. As the dragon approaches adulthood, its color deepens slowly to a darker, rich bronze tone. Very old dragons develop a blue-black tint to the edges of their scales. Powerful swimmers, they have webbed feet and smooth, flat scales. The pupils of its eyes fade as a dragon ages, until in the oldest the eyes resemble glowing green orbs.]],
+	ai = "human_level", ai_state = { talent_in=3, },
+    stats = { str=13, dex=10, con=13, int=14, wis=15, cha=14, luc=10 },
+    combat = { dam= {1,6} },
+    name = "copper dragon",
+    level_range = {3, nil}, exp_worth = 900,
+    rarity = 25,
+    max_life = resolvers.rngavg(42,48),
+    hit_die = 6,
+    challenge = 3,
+    infravision = 4,
+    combat_natural = 5,
+ 	--TODO: skills
+	movement_speed = 1.33,
+	alignment = "Chaotic Good",
+	resolvers.wounds()
+}
+
+--Immunity to fire, vulnerability to cold, water breathing
+newEntity{
+	define_as = "BASE_NPC_DRAGON_GOLD",
+	type = "dragon",
+	display = 'D', color=colors.GOLD,
+	body = { INVEN = 10 },
+	desc = [[On hatching, a gold dragon�s scales are dark yellow with golden metallic flecks. The flecks get larger as the dragon matures until, at the adult stage, the scales are completely golden. Gold dragons� faces are bewhiskered and sagacious; as they age, their pupils fade until the eyes resemble pools of molten gold.]],
+	ai = "human_level", ai_state = { talent_in=3, },
+    stats = { str=17, dex=10, con=15, int=14, wis=15, cha=14, luc=10 },
+    combat = { dam= {1,6} },
+    name = "gold dragon",
+    level_range = {3, nil}, exp_worth = 900,
+    rarity = 35,
+    max_life = resolvers.rngavg(65,70),
+    hit_die = 8,
+    challenge = 3,
+    infravision = 4,
+    combat_natural = 7,
+ 	--TODO: skills
+	movement_speed = 1.33,
+	alignment = "Lawful Good",
+	resolvers.wounds()
+}
+
+--Immunity to acid and cold, cloud walking, vulnerability to fire
+newEntity{
+	define_as = "BASE_NPC_DRAGON_SILVER",
+	type = "dragon",
+	display = 'D', color=colors.ANTIQUE_WHITE,
+	body = { INVEN = 10 },
+	desc = [[A silver wyrmling�s scales are blue-gray with silver highlights. As the dragon approaches adulthood, its color gradually brightens until the individual scales are scarcely visible.]],
+	ai = "human_level", ai_state = { talent_in=3, },
+    stats = { str=13, dex=10, con=13, int=14, wis=15, cha=14, luc=10 },
+    combat = { dam= {1,6} },
+    name = "silver dragon",
+    level_range = {3, nil}, exp_worth = 900,
+    rarity = 35,
+    max_life = resolvers.rngavg(48,55),
+    hit_die = 7,
+    challenge = 3,
+    infravision = 4,
+    combat_natural = 6,
+ 	--TODO: skills
+	movement_speed = 1.33,
+	alignment = "Lawful Good",
+	resolvers.wounds()
+}
