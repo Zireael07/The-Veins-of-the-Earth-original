982b11b364d9e6f1d1cde79d03e71bc605ac4ebc
Zireael07 <zi@mail.com>
2015-05-29 20:20:33 +0200

    new zone: kobold warrens

---
+++ data/zones/kobold warrens/npcs.lua
@@ -0,0 +1,21 @@
+-- Veins of the Earth
+-- Zireael 2015
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+load("/data/general/npcs/humanoid.lua")
+
+--Encounters are dummy npcs
+--load("/data/general/npcs/encounters/encounters_generic.lua")
+--load("/data/general/npcs/encounters/encounters_specific.lua")
