a30adfb2a8ab111494468d6902153f4c01c0f6ac
Zireael07 <zi@mail.com>
2015-08-14 10:37:27 +0200

    change debug create item dialog to use a tree list instead

--- dialogs/debug/CreateItem2.lua
+++ dialogs/debug/CreateItem2.lua
@@ -8,6 +8,7 @@ local Dialog = require 'engine.ui.Dialog'
 local List = require 'engine.ui.List'
 local Button = require 'engine.ui.Button'
 local Numberbox = require 'engine.ui.Numberbox'
+local TreeList = require "engine.ui.TreeList"
 
 local Ego = require 'mod.class.Ego'
 
@@ -18,21 +19,11 @@ function _M:init()
 
   Dialog.init(self, 'Create Object', 800, 600)
 
-  self:generateListCategory()
---  self:generateList()
+    self:generateTree()
 
---new
-  self.c_category = List.new {
-  width = 100,
-  nb_items = 20,
-  display_prop = 'display',
-  list = self.list_category,
-  scrollbar = true,
-  fct = function(item) self:selectItem(item) end,--self:selectCategory(item) end,
-}
 
 --old
-  self.c_items = List.new {
+--[[  self.c_items = List.new {
     width = 200,
     nb_items = 20,
     display_prop = 'display',
@@ -40,7 +31,17 @@ function _M:init()
 --    list = self.list,
     scrollbar = true,
     fct = function(item) self:selectItem(item) end,
+  }]]
+
+  self.c_tree = TreeList.new{width=250, height=self.ih*0.8, scrollbar=true, columns={
+      {width=100, display_prop="name"},
+  }, tree=self.all_items,
+      fct=function(item, sel, v) self:treeUse(item, sel, v) end,
+      select=function(item, sel) end, --self:on_select(item) end,
+      on_expand=function(item) end,
+      on_drawitem=function(item) end,
   }
+
   self.c_pfx_egos = List.new {
     width = 200,
     nb_items = 15,
@@ -76,9 +77,8 @@ function _M:init()
 
 
   self:loadUI {
-    {left=0, top=0, ui=self.c_category},
-    { left=self.c_category.w, top=0, ui=self.c_items},
-    { left=0, top=self.c_items.h, ui=self.c_nitems },
+    {left=0, top=0, ui=self.c_tree},
+    { left=0, top=self.c_tree.h, ui=self.c_nitems },
     { right=0, top=0, ui=self.c_sfx_egos },
     { right=self.c_sfx_egos.w, top=0, ui=self.c_pfx_egos },
     { right=0, bottom=0, ui=self.c_close },
@@ -127,13 +127,10 @@ function _M:selectItem(item)
     self.c_items:drawItem(self.sel_item)
   end
   self:updateButton()
-  if not self.sel_item.unique and not self.sel_item.type then
+  if not self.sel_item.unique then --and not self.sel_item.type then
   self:setupEgoLists()
   end
 
-  if self.sel_item.type then
-      self:setupItems()
- end
 end
 
 function _M:setupEgoLists()
@@ -181,7 +178,7 @@ function _M:createItem()
   if not self:readyToCreate() then return end
 
     local qty = self.c_nitems.number
-  --  local o = game.zone:finishEntity(game.level, 'object', self.sel_item.e)
+
     local o = game.zone:makeEntity(game.level, "object", {name = self.sel_item.name, ego_chance=-1000}, nil, true)
     o:setNumber(qty)
 
@@ -195,6 +192,69 @@ function _M:createItem()
     game.zone:addEntity(game.level, o, 'object', game.player.x, game.player.y)
 	game.log('Created '..o.name)
 end
+
+function _M:treeUse(item, sel, v)
+	if not item then return end
+	if item.nodes then
+		for _, other in ipairs(self.c_tree.tree) do
+			if other.shown then self.c_tree:treeExpand(false, other) end
+		end
+		self.c_tree:treeExpand(true, item)
+	elseif item.e then
+	--	self:use(item)
+        self:selectItem(item)
+	end
+end
+
+--Generate item tree
+function _M:generateTree()
+	local player = game.player
+	local oldtree = {}
+	for i, t in ipairs(self.all_items or {}) do oldtree[t.id] = t.shown end
+
+	local tree = {}
+	local newsel = nil
+	for i, e in ipairs(game.zone.object_list) do
+				local nodes = {}
+
+                if e.name and e.rarity and e.level_range then
+                    if #nodes == 0 or (nodes[type] and not nodes[type] == e.type) then --or (nodes[type] and not table.same_values(table.values(nodes.type), e.type)) then
+                    local display = (e.unique and '#ffd700#' or '#ffffff#') .. e.name .. '#LAST#'
+						nodes[#nodes+1] = {
+							name = e.name,
+                            type = e.type,
+                            display = display,
+                            unique = e.unique,
+							e = e
+						}
+
+						if self.sel_feat and self.sel_feat.type == sd.type then newsel = nodes[#nodes] end
+                    end
+                end
+
+				if #nodes > 0 then
+					tree[#tree+1] = {
+						name = e.type,
+                        display = e.type,
+						id = e.type,
+						shown = oldtree[e.type],
+						nodes = nodes,
+					}
+				end
+	end
+
+	self.all_items = tree
+	if self.c_tree then
+		self.c_tree.tree = self.all_items
+		self.c_tree:generate()
+		if newsel then self:treeUse(newsel)
+		else
+			self.sel_feat = nil
+		end
+	end
+end
+
+
 
 --Generate item list
 function _M:generateList(type)
@@ -229,7 +289,7 @@ end
 
 --Generate categories
 --Needs filter badly!!!
-function _M:generateListCategory()
+--[[function _M:generateListCategory()
     local list = {}
 
     for _, o in ipairs(game.zone.object_list) do
@@ -270,4 +330,4 @@ function _M:setupItems()
         self.c_items.list = self.list
         self.c_items:generate()
     end
-end
+end]]
