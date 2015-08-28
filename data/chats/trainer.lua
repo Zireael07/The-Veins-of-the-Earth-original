bd1c3f22e790bb9d34b8b5bff5b9e7120fab30a6
Zireael07 <zi@mail.com>
2015-08-28 19:46:48 +0200

    begin work on (optional) trainers

---
+++ data/chats/trainer.lua
@@ -0,0 +1,54 @@
+newChat{id="start",
+    text=[[Welcome! Maybe you need me to train you?]],
+    answers = {
+        {[[No, I have no need of your services.]], cond=function(npc, player)
+        if player:getInt() < 10 then return end
+        return true
+    end},
+        {[[Me no need train.]], cond=function(npc, player)
+        if player:getInt() >= 10 then return end
+        return true
+    end},
+        {[[Yes, I'd like to train a class.]], action = function(npc, player)
+            local price = 400 --2 gold
+            if price > player.money then require("engine.ui.Dialog"):simplePopup("Not enough money", "This costs 100 silver, you need more money.") return end
+
+            player:incMoney(-price)
+            game:registerDialog(require("mod.dialogs.ClassLevelupDialog").new(player))
+
+
+        end, cond=function(npc, player)
+            if player.money < 400 then return end
+            return true
+        end
+        },
+        {[[Yes, I'd like to train a skill.]], action = function(npc, player)
+            local price = 50
+            if price > player.money then require("engine.ui.Dialog"):simplePopup("Not enough money", "This costs 600 silver, you need more money.") return end
+
+            player:incMoney(-price)
+            game:registerDialog(require("mod.dialogs.SkillDialog").new(player))
+
+
+        end, cond=function(npc, player)
+            if player.money < 50 then return end
+            return true
+        end
+        },
+        {[[Yes, I'd like to train a feat.]], action = function(npc, player)
+            local price = 100
+            if price > player.money then require("engine.ui.Dialog"):simplePopup("Not enough money", "This costs 2800 silver, you need more money.") return end
+
+            player:incMoney(-price)
+            game:registerDialog(require("mod.dialogs.FeatDialog").new(player))
+
+        end, cond=function(npc, player)
+            if player.money < 100 then return end
+            return true
+        end
+        },
+        {[[Back away.]], action = function(npc, player) player:displace(npc) end    },
+    },
+}
+
+return "start"
---
+++ data/general/npcs/variant/trainers.lua
@@ -0,0 +1,67 @@
+--Veins of the Earth
+--Zireael 2015
+
+local Talents = require("engine.interface.ActorTalents")
+
+--local class_table = { "barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "wizard", "warlock", "shaman" }
+local class_table = { "barbarian", "bard", "cleric", "druid", "fighter", "ranger", "rogue", "sorcerer", "wizard", "shaman" }
+local class_pick = rng.table(class_table)
+local resolve = {class_pick=4}
+
+newEntity{
+    define_as = "BASE_NPC_TRAINER",
+    type = "humanoid",
+    display = 'h',
+    level_range = {1, nil},
+    max_life = resolvers.rngavg(5,8),
+    stats = { str=13, dex=13, con=12, int=12, wis=9, cha=10, luc=10 },
+    hit_die = 1,
+    challenge = 1,
+    alignment = "Neutral",
+    languages = {"Common", "Undercommon"},
+
+    body = { INVEN = 10, MAIN_HAND = 1, OFF_HAND = 1, BODY = 1, HELM = 1, QUIVER = 1 },
+    ai = "humanoid_level", ai_state = { talent_in=1, ai_move="move_astar", },
+    combat = { dam= {1,6} },
+    faction = "neutral",
+    open_door = true,
+    show_portrait = true,
+
+    resolvers.equip{
+        full_id=true,
+        { name = "chainmail", not_properties={"cursed"}, veins_drops={"npc"}, add_levels=4, veins_level=resolvers.npc_drops_level  },
+        { name = "light metal shield", not_properties={"cursed"}, veins_drops={"npc"}, add_levels=4, veins_level=resolvers.npc_drops_level  },
+        { name = "longsword", not_properties={"cursed"}, veins_drops={"npc"}, add_levels=4, veins_level=resolvers.npc_drops_level  },
+        { name = "arrows", not_properties={"cursed"}, veins_drops={"npc"}, add_levels=4, veins_level=resolvers.npc_drops_level  },
+    },
+    resolvers.inventory {
+    full_id=true,
+    { name = "shortbow", add_levels=4  },
+    { name = "food ration" },
+    },
+    resolvers.classes{fighter=2},
+--    resolvers.classes{resolve}
+    can_talk = "trainer",
+
+    resolvers.talents{ [Talents.T_SHOOT]=1,
+    --give the simple weapon proficiency warrior/fighter/wizard class all have
+    [Talents.T_SIMPLE_WEAPON_PROFICIENCY]=1,
+    },
+    resolvers.wounds()
+}
+
+newEntity{ base = "BASE_NPC_TRAINER",
+    name = "human trainer",
+    subtype = "human",
+    color=colors.WHITE,
+    image = "tiles/new/human_fighter.png",
+    rarity = 5,
+}
+
+newEntity{ base = "BASE_NPC_TRAINER",
+    name = "drow trainer",
+    subtype = "drow",
+    color=colors.BLACK,
+    image = "tiles/new/drow_fighter.png",
+    rarity = 5,
+}
