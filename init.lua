cab60a46360bd23718da46f929e4d9e1fec041b6
Zireael07 <zi@mail.com>
2015-09-01 16:02:30 +0200

    bump engine version

--- init.lua
+++ init.lua
@@ -20,7 +20,7 @@ short_name = "veins"
 author = { "Zireael", "x" }
 homepage = "http://veins-of-the-earth.wikidot.com/"
 version = {0,30,0}
-engine = {1,3,0,"te4"}
+engine = {1,3,1,"te4"}
 description = [[
 #SANDY_BROWN# BETA 10#LAST#
 In DarkGod's words, #GOLD#"a fantasy d20-themed dungeon crawler"#LAST#.
