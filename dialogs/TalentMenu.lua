b42eb36c01247a09cd4c7e59c5304af61e3560b6
Zireael07 <zi@mail.com>
2015-09-17 14:23:03 +0200

    clarifying comment

--- dialogs/TalentMenu.lua
+++ dialogs/TalentMenu.lua
@@ -13,9 +13,9 @@
 --
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
--- Nicolas Casalini "DarkGod"
--- darkgod@te4.org
+
+
+--For the menu buttons
 
 require "engine.class"
 local Dialog = require "engine.ui.Dialog"
