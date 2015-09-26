8a851ec2fe6b376005e98b9a39f83a6c2b8dd78e
Zireael07 <zi@mail.com>
2015-09-26 17:20:22 +0200

    add lore to kraken and roc

--- data/general/npcs/animals_other.lua
+++ data/general/npcs/animals_other.lua
@@ -8,6 +8,10 @@ newEntity{
         display = 'b', color=colors.GOLD,
         body = { INVEN = 10 },
         desc = [[An immense bird.]],
+        specialist_desc = [[Most rocs have dark brown or golden plumage. Red, black, or white rocs are said to exist, but sighting such a bird is an ill omen.]],
+        uncommon_desc = [[Rocs usually hunt within 10 miles of their massive, mountaintop aeries. If a snatched creature offers little resistance, rocs sometimes carry it back to their nests to feed their hatchlings.]],
+        common_desc = [[Rocs prey upon lesser creatures much like raptors a fraction their size. Their preferred tactic is to swoop low, use their talons to snatch up a creature—which might be as big as an elephant—and then carry the hapless prey hundreds of feet into the air before flinging it back to earth.]],
+        base_desc = "This unbelievably huge raptor is a roc, a bird of immense size and strength.",
 
         ai = "dumb_talented_simple", ai_state = { talent_in=3, },
         stats = { str=34, dex=15, con=24, int=2, wis=13, cha=11, luc=12 },
--- data/general/npcs/aquatic.lua
+++ data/general/npcs/aquatic.lua
@@ -8,11 +8,15 @@ local Talents = require("engine.interface.ActorTalents")
 --Spell-likes: 1/day—control weather, control winds, dominate animal (DC 18), resist energy.
 newEntity{
 	define_as = "BASE_NPC_KRAKEN",
-	type = "magical beast", subtype = "aquatic"
+	type = "magical beast", subtype = "aquatic",
 --	image = "tiles/mobiles/kraken.png",
 	display = 'B', color=colors.DARK_BLUE,
 	body = { INVEN = 10 },
 	desc = [[An immense kraken.]],
+	specialist_desc = [[A kraken can control weather and control winds as spell-like abilities, turning the winds and sea itself against its hapless victims. Krakens often rule vast undersea kingdoms, far beneath the waves.]],
+	uncommon_desc = [[A kraken will usually retreat if several of its tentacles are severed. Lost tentacles eventually grow back. A kraken can cover its retreat by ejecting clouds of murky black ink, and can jet through the water at high speeds. A kraken can also use resist energy as a spell-like ability to protect itself.]],
+	common_desc = [[A kraken possesses malign pride and intelligence to match its incredible physical prowess. A kraken uses its two massive, barbed tentacles to snatch sailors off the decks of their ships, then crushes them with its eight arms before devouring them with a beak strong enough to bite through steel. In some cases, a kraken may simply crush the ship itself to splinters, picking off survivors as they try to save themselves. A kraken can also turn the sea’s denizens against its opponents by mentally dominating sea animals and pressing intelligent aquatic creatures into slavery. Krakens speak Common and Aquan.]],
+	base_desc = [[This brutish and gigantic squid is a kraken, a beast of legendary power. Krakens plunder ships in search of food and wealth, which they collect in coral palaces deep underwater.]],
 
 	ai = "dumb_talented_simple", ai_state = { talent_in=3, },
 	stats = { str=34, dex=10, con=29, int=21, wis=20, cha=20, luc=10 },
