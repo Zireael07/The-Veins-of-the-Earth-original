2ae9a583a406bf8c716fa4532b521b6fb3dd29c2
Zireael07 <zi@mail.com>
2015-10-23 17:35:03 +0200

    drow village area

---
+++ data/zones/drow village/npcs.lua
@@ -0,0 +1,33 @@
+-- Veins of the Earth
+-- Zireael 2015
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+--Encounters are dummy npcs
+--load("/data/general/npcs/encounters/encounters_generic.lua")
+--load("/data/general/npcs/encounters/encounters_specific.lua")
+
+--Neutrals
+load("/data/general/npcs/neutral.lua", rarity(5))
+
+load("/data/general/npcs/townies_drow.lua", rarity(0))
+
+if config.settings.veins.training then
+    load("/data/general/npcs/variant/trainers.lua")
+end
+
+--Neutral vermin, elementals, fiends
+load("/data/general/npcs/vermin.lua", function(e) e.faction = "neutral" end, rarity(1))
+load("/data/general/npcs/elementals.lua", function(e) e.faction = "neutral" end, rarity(18))
+load("/data/general/npcs/outsider_evil.lua", function(e) e.faction = "neutral" end, rarity(20))
