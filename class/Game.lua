e6a68e186c9dab5302454b2c4cc4299df8f1930e
Zireael07 <zi@mail.com>
2015-12-16 20:02:49 +0100

    fix item feeling logic

--- class/Game.lua
+++ class/Game.lua
@@ -673,8 +673,8 @@ function _M:changeLevel(lev, zone)
 			end
 		end end
 
-		if max_magic > 2 then item_feeling = "You get goosebumps on your skin. A magic item is radiating power."
-		elseif max_magic > 4 then item_feeling = "The feeling of power threatens to overwhelm you. A powerful magic item must be nearby."
+		if max_magic > 4 then item_feeling = "The feeling of power threatens to overwhelm you. A powerful magic item must be nearby."
+		elseif max_magic > 2 then item_feeling = "You get goosebumps on your skin. A magic item is radiating power."
 		else item_feeling = "You get a feeling that there is only junk here."
 		end
 
