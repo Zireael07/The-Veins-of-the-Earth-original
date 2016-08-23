cf58dc306750780c612eb35c371b7ed7f39d3404
Zireael07 <zi@mail.com>
2016-08-23 14:55:13 +0200

    fix colors in intro message

--- data/texts/intro-outcast.lua
+++ data/texts/intro-outcast.lua
@@ -1,6 +1,8 @@
 return [[You have been cast out of your birth city and house because of your ideals. 
-Brave adventurer, you are now lost in the underground corridors of the #LIGHT_BROWN#Veins of the Earth#LAST#. There is no way to return to your homeland.
-How long can you survive?
+
+Brave adventurer, you are now lost in the underground corridors of the #SANDY_BROWN#Veins of the Earth#LAST#. There is no way to return to your homeland.
+
+How long can you survive...?
 
 Press ESC or click to close.
 
